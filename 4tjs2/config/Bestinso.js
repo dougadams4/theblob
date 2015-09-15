05922c5dd3f52037033d055abaa8b092104d2bfd
dougadams4 <dougadams4@gmail.com>
2015-09-15 01:04:29 -0700

    snapshot4tjs2Bestinso.js - initial pull

--- 4tjs2/config/Bestinso.js
+++ 4tjs2/config/Bestinso.js
@@ -16,193 +16,219 @@
             siteEnable: true
         },
         PageSettings: {
-            Home: {
-                tout1: {
-                    enable: true,
-                    resultType: "1,4",
-                    numItems: "6,6",
-                    caption: "Top Sellers",
-                    productStyle: "product4T product4THome",
-                    divSelect: "#Table_01",
-                    divPosition: "replace",
-                    carousel: {
-                        numVis: 3,
-                        circular: true
-                    },
-                    showRatings: true,
-                    showBuyButton: false,
-                    imageSize: "&maxx=130&maxy=130",
-                    maxImageHeight: 180,
-                    wrapper: "<table width='100%' class='PD14T'><tr><td></td></tr></table>",
-                    inCart: false
+            Home: [{
+                enable: true,
+                resultType: "1,4",
+                numItems: "6,6",
+                caption: "Top Sellers",
+                productStyle: "product4T product4THome",
+                divSelect: "#Table_01",
+                divPosition: "replace",
+                carousel: {
+                    numVis: 3,
+                    circular: true
                 },
-                tout2: {
-                    enable: true,
-                    resultType: 4,
-                    numItems: 3,
-                    caption: "Customers also viewed...",
-                    showCaption: false,
-                    productStyle: "product4T product4TPD1",
-                    divSelect: ".hotprod  tr:last",
-                    divPosition: "replace",
-                    imageSize: "&maxx=120&maxy=120",
-                    maxImageHeight: 220,
-                    showRatings: true,
-                    showBuyButton: false,
-                    wrapper: "<tr class='PD14T'><td></td></tr>",
-                    inCart: false
-                }
-            },
-            ProductDetail: {
-                tout1: {
-                    enable: true,
-                    resultType: 0,
-                    numItems: 3,
-                    caption: "Customers also bought...",
-                    showCaption: false,
-                    productStyle: "product4T product4TPD1",
-                    divSelect: ".hotprod tr:last",
-                    divPosition: "replace",
-                    showRatings: true,
-                    showBuyButton: false,
-                    imageSize: "&maxx=120&maxy=120",
-                    maxImageHeight: 160,
-                    wrapper: "<tr class='PD14T'><td></td></tr>",
-                    inCart: false
+                showRatings: true,
+                showBuyButton: false,
+                imageSize: "&maxx=130&maxy=130",
+                maxImageHeight: 180,
+                wrapper: "<table width='100%' class='PD14T'><tr><td></td></tr></table>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.bestinsoles.com/, "");
+                    }
                 },
-                tout2: {
-                    enable: true,
-                    resultType: 3,
-                    numItems: 6,
-                    caption: "Customers also viewed...",
-                    productStyle: "product4T product4TPD2",
-                    divSelect: "#REL4T a[name=Review]",
-                    divPosition: "above",
-                    carousel: {
-                        numVis: 3,
-                        circular: true
-                    },
-                    imageSize: "&maxx=120&maxy=120",
-                    maxImageHeight: 220,
-                    showRatings: true,
-                    showBuyButton: false,
-                    wrapper: "<tr><td><div class='PD24T'></div></td></tr>",
-                    rawJS: {
-                        preDisplay: function (tout) {
-                            $(tout.divSelect).prevAll("div").hide();
-                        }
-                    },
-                    inCart: false
-                }
-            },
-            Search: {
-                tout1: {
-                    enable: true,
-                    resultType: 0,
-                    numItems: 3,
-                    caption: "Customers also bought...",
-                    showCaption: false,
-                    productStyle: "product4T product4TPD1",
-                    divSelect: ".hotprod tr:last",
-                    divPosition: "replace",
-                    showRatings: true,
-                    showBuyButton: false,
-                    imageSize: "&maxx=120&maxy=120",
-                    maxImageHeight: 160,
-                    wrapper: "<tr class='PD14T'><td></td></tr>",
-                    inCart: false
-                }
-            },
-            Category: {
-                tout1: {
-                    enable: true,
-                    resultType: 5,
-                    numItems: 12,
-                    caption: "Related Top Sellers",
-                    productStyle: "product4T product4TCat",
-                    divSelect: "#Table3",
-                    divPosition: "above",
-                    carousel: {
-                        numVis: 3,
-                        circular: true
-                    },
-                    imageSize: "&maxx=100&maxy=100",
-                    maxImageHeight: 100,
-                    showRatings: true,
-                    showBuyButton: false,
-                    wrapper: "<div id='CAT4T'></div>",
-                    rawJS: {
-                        preInit: function (tout) {
-                            tout.enable = tout.enable && $("table.alternative").length >= 20;
-                            if ($("#info-pain-div").length) {
-                                tout.divPosition = "below";
-                                tout.enable = true;
-                                tout.divSelect = "td.data > table";
-                            }
-                        }
-                    },
-                    inCart: false
+                inCart: false
+            }, {
+                enable: true,
+                resultType: 4,
+                numItems: 3,
+                caption: "Customers also viewed...",
+                showCaption: false,
+                productStyle: "product4T product4TPD1",
+                divSelect: ".hotprod  tr:last",
+                divPosition: "replace",
+                imageSize: "&maxx=120&maxy=120",
+                maxImageHeight: 220,
+                showRatings: true,
+                showBuyButton: false,
+                wrapper: "<tr class='PD14T'><td></td></tr>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.bestinsoles.com/, "");
+                    }
                 },
-                tout2: {
-                    enable: true,
-                    resultType: 0,
-                    numItems: 3,
-                    caption: "Customers also viewed...",
-                    showCaption: false,
-                    productStyle: "product4T product4TPD1",
-                    divSelect: ".hotprod  tr:last",
-                    divPosition: "replace",
-                    imageSize: "&maxx=120&maxy=120",
-                    maxImageHeight: 220,
-                    showRatings: true,
-                    showBuyButton: false,
-                    wrapper: "<tr class='PD14T'><td></td></tr>",
-                    inCart: false
-                }
-            },
-            AddToCart: {
-                tout1: {
-                    enable: true,
-                    resultType: 0,
-                    numItems: 12,
-                    caption: "You may also like...",
-                    productStyle: "product4T product4TVC",
-                    divSelect: "tr:has(img[src*=Amazon]):last td:last",
-                    divPosition: "above",
-                    carousel: {
-                        numVis: 4,
-                        circular: true
-                    },
-                    imageSize: "&maxx=100&maxy=100",
-                    maxImageHeight: 180,
-                    showRatings: true,
-                    showBuyButton: false,
-                    wrapper: "<td colspan='6'><div class='VC4T'></div></td>",
-                    rawJS: {
-                        preDisplay: function (tout) {
-                            $(tout.divSelect).siblings(":empty").remove();
-                            $(tout.divSelect).attr("valign", "top");
-                        }
-                    },
-                    inCart: true
+                inCart: false
+            }],
+            ProductDetail: [{
+                enable: true,
+                resultType: 0,
+                numItems: 3,
+                caption: "Customers also bought...",
+                showCaption: false,
+                productStyle: "product4T product4TPD1",
+                divSelect: ".hotprod tr:last",
+                divPosition: "replace",
+                showRatings: true,
+                showBuyButton: false,
+                imageSize: "&maxx=120&maxy=120",
+                maxImageHeight: 160,
+                wrapper: "<tr class='PD14T'><td></td></tr>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.bestinsoles.com/, "");
+                    }
                 },
-                tout2: {
-                    enable: true,
-                    resultType: 4,
-                    numItems: 3,
-                    caption: "Customers also viewed...",
-                    showCaption: false,
-                    productStyle: "product4T product4TPD1",
-                    divSelect: ".hotprod  tr:last",
-                    divPosition: "replace",
-                    imageSize: "&maxx=120&maxy=120",
-                    maxImageHeight: 220,
-                    showRatings: true,
-                    showBuyButton: false,
-                    wrapper: "<tr class='PD14T'><td></td></tr>",
-                    inCart: false
-                }
+                inCart: false
+            }, {
+                enable: true,
+                resultType: 3,
+                numItems: 6,
+                caption: "Customers also viewed...",
+                productStyle: "product4T product4TPD2",
+                divSelect: "#REL4T a[name=Review]",
+                divPosition: "above",
+                carousel: {
+                    numVis: 3,
+                    circular: true
+                },
+                imageSize: "&maxx=120&maxy=120",
+                maxImageHeight: 220,
+                showRatings: true,
+                showBuyButton: false,
+                wrapper: "<tr><td><div class='PD24T'></div></td></tr>",
+                rawJS: {
+                    preDisplay: function (tout) {
+                        $(tout.divSelect).prevAll("div").hide();
+                    },
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.bestinsoles.com/, "");
+                    }
+                },
+                inCart: false
+            }],
+            Search: [{
+                enable: true,
+                resultType: 0,
+                numItems: 3,
+                caption: "Customers also bought...",
+                showCaption: false,
+                productStyle: "product4T product4TPD1",
+                divSelect: ".hotprod tr:last",
+                divPosition: "replace",
+                showRatings: true,
+                showBuyButton: false,
+                imageSize: "&maxx=120&maxy=120",
+                maxImageHeight: 160,
+                wrapper: "<tr class='PD14T'><td></td></tr>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.bestinsoles.com/, "");
+                    }
+                },
+                inCart: false
             }
+            ],
+            Category: [{
+                enable: true,
+                resultType: 5,
+                numItems: 12,
+                caption: "Related Top Sellers",
+                productStyle: "product4T product4TCat",
+                divSelect: "#Table3",
+                divPosition: "above",
+                carousel: {
+                    numVis: 3,
+                    circular: true
+                },
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                showBuyButton: false,
+                wrapper: "<div id='CAT4T'></div>",
+                rawJS: {
+                    preInit: function (tout) {
+                        tout.enable = tout.enable && $("table.alternative").length >= 20;
+                        if ($("#info-pain-div").length) {
+                            tout.divPosition = "below";
+                            tout.enable = true;
+                            tout.divSelect = "td.data > table";
+                        }
+                    },
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.bestinsoles.com/, "");
+                    }
+                },
+                inCart: false
+            }, {
+                enable: true,
+                resultType: 0,
+                numItems: 3,
+                caption: "Customers also viewed...",
+                showCaption: false,
+                productStyle: "product4T product4TPD1",
+                divSelect: ".hotprod  tr:last",
+                divPosition: "replace",
+                imageSize: "&maxx=120&maxy=120",
+                maxImageHeight: 220,
+                showRatings: true,
+                showBuyButton: false,
+                wrapper: "<tr class='PD14T'><td></td></tr>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.bestinsoles.com/, "");
+                    }
+                },
+                inCart: false
+            }],
+            AddToCart: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "You may also like...",
+                productStyle: "product4T product4TVC",
+                divSelect: "tr:has(img[src*=Amazon]):last td:last",
+                divPosition: "above",
+                carousel: {
+                    numVis: 4,
+                    circular: true
+                },
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 180,
+                showRatings: true,
+                showBuyButton: false,
+                wrapper: "<td colspan='6'><div class='VC4T'></div></td>",
+                rawJS: {
+                    preDisplay: function (tout) {
+                        $(tout.divSelect).siblings(":empty").remove();
+                        $(tout.divSelect).attr("valign", "top");
+                    },
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.bestinsoles.com/, "");
+                    }
+                },
+                inCart: true
+            }, {
+                enable: true,
+                resultType: 4,
+                numItems: 3,
+                caption: "Customers also viewed...",
+                showCaption: false,
+                productStyle: "product4T product4TPD1",
+                divSelect: ".hotprod  tr:last",
+                divPosition: "replace",
+                imageSize: "&maxx=120&maxy=120",
+                maxImageHeight: 220,
+                showRatings: true,
+                showBuyButton: false,
+                wrapper: "<tr class='PD14T'><td></td></tr>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.bestinsoles.com/, "");
+                    }
+                },
+                inCart: false
+            }]
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
