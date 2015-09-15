7f69140d80d33740528109588f02682a443c390f
dougadams4 <dougadams4@gmail.com>
2015-09-15 02:21:01 -0700

    live4tjs5DiscoNew.js - initial pull

---
+++ 4tjs5/config/DiscoNew.js
@@ -0,0 +1,132 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.CONFIG = {
+        SiteInfo: {
+            baseURL: "www.discountnewagebooks.com",
+            alias: "DiscoNew",
+            GA_UA: "UA-604429-1",
+            platform: "4Tell3d.js",
+            addCartBtnAtts: "input type='button' value='Add to Cart' class='btn' onmouseout=this.className='btn' onmouseover=this.className='btn_over'",
+            addCartImage: "",
+            spacerImage: "/web/assets/images/spacer.gif",
+            emptyImage: "thumbnail.asp?file=/assets/images/default.jpg",
+            priceClass: "price2",
+            pricePrefix: "Our Price: ",
+            salePricePrefix: "On Sale: ",
+            includeBase: false,
+            siteEnable: true
+        },
+        PageSettings: {
+            Home: {
+                tout1: {
+                    enable: true,
+                    resultType: 1,
+                    numItems: 16,
+                    caption: "Our customers also bought...",
+                    productStyle: "product4T product4THome",
+                    divSelect: "#midContent tr:first",
+                    divPosition: "below",
+                    carousel: {
+                        numVis: 4,
+                        circular: true
+                    },
+                    numRows: 2,
+                    showRatings: false,
+                    maxImageHeight: 160,
+                    imageSize: "&maxx=120&maxy=120",
+                    wrapper: "<tr><td><div class='HOME4T'></div></td></tr>",
+                    inCart: false
+                }
+            },
+            ProductDetail: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 12,
+                    caption: "Customers also bought...",
+                    productStyle: "product4T product4TPD1",
+                    divSelect: "tr:has(tr #listing_main_image_link)",
+                    divPosition: "below",
+                    carousel: {
+                        numVis: 3,
+                        circular: true
+                    },
+                    showRatings: false,
+                    maxImageHeight: 150,
+                    imageSize: "&maxx=120&maxy=120",
+                    wrapper: "<tr><td colspan='2'><div class='PD14T'></div></td></tr>",
+                    inCart: false
+                },
+                tout2: {
+                    enable: true,
+                    resultType: 3,
+                    numItems: 8,
+                    caption: "Customers also viewed...",
+                    productStyle: "product4T product4TPD2",
+                    divSelect: "#REL4T",
+                    divPosition: "replace",
+                    carousel: {
+                        numVis: 4,
+                        circular: true
+                    },
+                    imageSize: "&maxx=120&maxy=120",
+                    maxImageHeight: 120,
+                    showRatings: false,
+                    wrapper: "<div class='PD14T'></div>",
+                    inCart: false
+                }
+            },
+            Search: {
+                tout1: {
+                    enable: false
+                }
+            },
+            Category: {
+                tout1: {
+                    enable: true,
+                    resultType: 5,
+                    numItems: 12,
+                    caption: "Customers also bought...",
+                    productStyle: "product4T product4TCat",
+                    divSelect: "#frmsortby tr:has(tr tr tr td.titles)",
+                    divPosition: "above",
+                    carousel: {
+                        numVis: 4,
+                        circular: true
+                    },
+                    showRatings: false,
+                    maxImageHeight: 150,
+                    imageSize: "&maxx=120&maxy=120",
+                    wrapper: "<tr><td colspan='2'><div class='PD14T'></div></td></tr>",
+                    rawJS: { preInit: function (tout) { tout.enable = tout.enable && $("table.alternative").length > 15; } },
+                    inCart: false
+                }
+            },
+            AddToCart: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 12,
+                    caption: "You may also like...",
+                    showCaption: false,
+                    productStyle: "product4T product4TVC",
+                    divSelect: "table.alternative",
+                    divPosition: "replace",
+                    carousel: {
+                        numVis: 4,
+                        circular: true
+                    },
+                    imageSize: "&maxx=100&maxy=100",
+                    maxImageHeight: 100,
+                    showRatings: false,
+                    wrapper: "<div class='PD14T'></div>",
+                    rawJS: {
+                        preDisplay: function (tout) {
+                            $("td.checkout-headers").attr("class", "product4TCaption").text(tout.caption);
+                        }
+                    },
+                    inCart: true
+                }
+            }
+        }
+    }
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
