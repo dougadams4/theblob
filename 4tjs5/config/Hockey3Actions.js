7a8c15513d1c891d248e689f1de40aeedfb28862
dougadams4 <dougadams4@gmail.com>
2015-09-15 07:10:43 -0700

    live4tjs5Hockey3Actions.js - initial pull

---
+++ 4tjs5/config/Hockey3Actions.js
@@ -0,0 +1,27 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+
+    _4TellBoost.getBuyBtn = function (tout, itemdata) {
+        var buyWrapper = $("<div class='productBuy' />");
+        var buyBtn = $("<" + _4TellBoost.SiteInfo.addCartBtnAtts + " />");
+        if (_4TellBoost.SiteInfo.addCartImage) {
+            var buyBtnImage = '';
+            if (_4TellBoost.SiteInfo.includeBase)
+                buyBtnImage = '//' + _4TellBoost.SiteInfo.baseURL + '/';
+            buyBtnImage += _4TellBoost.SiteInfo.addCartImage;
+            buyBtn.attr("src", buyBtnImage);
+        }
+        if (buyBtn)
+            buyBtn.appendTo(buyWrapper);
+        var newAddress = '';
+        if (_4TellBoost.SiteInfo.includeBase)
+            newAddress += 'http://' + _4TellBoost.SiteInfo.baseURL; //link to PDP should not be https
+        newAddress += itemdata.pageLink;
+
+        //can't wrap with a-href in IE so add new address to the onclick handler
+        buyBtn.click(function () {
+            _4TellBoost.TrackClick(tout.toutType, itemdata.productID, newAddress);
+        });
+        return buyWrapper;
+    };
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
