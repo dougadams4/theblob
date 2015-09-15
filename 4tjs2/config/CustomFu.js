9e99a50fa1b3a135dcb78c4056021df4b0709ba4
dougadams4 <dougadams4@gmail.com>
2015-09-15 02:09:36 -0700

    snapshot4tjs2CustomFu.js - initial pull

--- 4tjs2/config/CustomFu.js
+++ 4tjs2/config/CustomFu.js
@@ -21,31 +21,33 @@
                     enable: false
                 }
             },
-            ProductDetail: {
-                tout1: {
-                    enable: false,
-                    resultType: 0,
-                    numItems: 10,
-                    caption: "Our customers also bought...",
-                    productStyle: "product4T product4TPD2",
-                    divSelect: "tr:has(tr img#large)",
-                    divPosition: "below",
-                    carousel: {
-                        numVis: 5,
-                        circular: true
-                    },
-                    imageSize: "&maxx=120&maxy=120",
-                    showRatings: true,
-                    maxImageHeight: 150,
-                    wrapper: "<tr><td colspan='2'></td></tr>",
-                    rawJS: {
-                        preDisplay: function () {
-                            //$("tr:has(tr .addthis_default_style):last").hide();
-                        }
-                    },
-                    inCart: false
+            ProductDetail: [{
+                enable: false,
+                resultType: 0,
+                numItems: 10,
+                caption: "Our customers also bought...",
+                productStyle: "product4T product4TPD2",
+                divSelect: "tr:has(tr img#large)",
+                divPosition: "below",
+                carousel: {
+                    numVis: 5,
+                    circular: true
                 },
-                tout2: {
+                imageSize: "&maxx=120&maxy=120",
+                showRatings: true,
+                maxImageHeight: 150,
+                wrapper: "<tr><td colspan='2'></td></tr>",
+                rawJS: {
+                    preDisplay: function () {
+                        //$("tr:has(tr .addthis_default_style):last").hide();
+                    },
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.customfurnitureworld.com/, "");
+                    }
+                },
+                inCart: false
+            },
+                {
                     enable: true,
                     resultType: 1,
                     numItems: 10,
@@ -63,11 +65,13 @@
                     rawJS: {
                         preDisplay: function () {
                             //$("ul#tablist li:last").hide();
-                        }
+                        },
+                        perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.customfurnitureworld.com/, "");
+                    }
                     },
                     inCart: false
-                }
-            },
+                }],
             Search: {
                 tout1: {
                     enable: false
@@ -78,33 +82,34 @@
                     enable: false
                 }
             },
-            AddToCart: {
-                tout1: {
-                    enable: true,
-                    resultType: 0,
-                    numItems: 12,
-                    caption: "You may also like...",
-                    productStyle: "product4T product4TVC",
-                    divSelect: "#BAR4T",
-                    divPosition: "append",
-                    carousel: {
-                        numVis: 3,
-                        circular: true
+            AddToCart: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "You may also like...",
+                productStyle: "product4T product4TVC",
+                divSelect: "#BAR4T",
+                divPosition: "append",
+                carousel: {
+                    numVis: 3,
+                    circular: true
+                },
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                rawJS: {
+                    preInit: function () {
+                        $('div#rightcol tr:has(tr input):last').after('<tr><td colspan="5" id="BAR4T"></td><td colspan="1" valign="top"><table id="FOO"></table></td></tr>');
+                        var foo = $('div#rightcol tr').filter(':has(tr a[href*=paypal_express]):last, :has(tr img[src*=google]):last, :has(tr input):last');
+                        $('#FOO').append(foo);
+                        $("tr:has(tr .checkout-headers)").hide();
                     },
-                    imageSize: "&maxx=100&maxy=100",
-                    maxImageHeight: 100,
-                    showRatings: true,
-                    rawJS: {
-                        preInit: function () {
-                            $('div#rightcol tr:has(tr input):last').after('<tr><td colspan="5" id="BAR4T"></td><td colspan="1" valign="top"><table id="FOO"></table></td></tr>');
-                            var foo = $('div#rightcol tr').filter(':has(tr a[href*=paypal_express]):last, :has(tr img[src*=google]):last, :has(tr input):last');
-                            $('#FOO').append(foo);
-                            $("tr:has(tr .checkout-headers)").hide();
-                        }
-                    },
-                    inCart: true
-                }
-            }
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink = itemdata.imageLink.replace(/^www.customfurnitureworld.com/, "");
+                    }
+                },
+                inCart: true
+            }]
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
