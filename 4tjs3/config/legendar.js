5a6a756141f7a9021de80a50dd2a40500e9bdc68
dougadams4 <dougadams4@gmail.com>
2015-09-15 07:54:50 -0700

    live4tjs3legendar.js - initial pull

---
+++ 4tjs3/config/legendar.js
@@ -0,0 +1,133 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.CONFIG = {
+        SiteInfo: {
+            baseURL: "www.legendaryusa.com",
+            alias: "legendar",
+            GA_UA: "UA-1557322-1",
+            platform: "4Tell3d.js",
+            addCartBtnAtts: "input type='button' value='Add to Cart' class='btn' onmouseout=this.className='btn' onmouseover=this.className='btn_over'",
+            pricePrefix: "",
+            salePricePrefix: "",
+            includeBase: false,
+            siteEnable: true
+        },
+        PageSettings: {
+            Home: [{
+                enable: true,
+                resultType: 1,
+                numItems: 12,
+                caption: "Recommended for you",
+                productStyle: "product4T product4TPD1",
+                divSelect: ".feat-item-list",
+                divPosition: "replace",
+                carousel: {
+                    items: 4,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                showRatings: true,
+                maxImageHeight: 150,
+                imageSize: "&maxx=180&maxy=180",
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
+                inCart: false
+            }],
+            ProductDetail: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "Customers also bought",
+                productStyle: "product4T product4TPD1",
+                divSelect: ".detail-section",
+                divPosition: "below",
+                carousel: {
+                    items: 3,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                showRatings: true,
+                imageSize: "&maxx=180&maxy=180",
+                maxImageHeight: 150,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
+                inCart: false
+            }, {
+                enable: true,
+                resultType: 3,
+                numItems: 12,
+                caption: "Customers also viewed",
+                productStyle: "product4T product4TPD2",
+                divSelect: ".relatedBlock, .accessoriesBlock",
+                divPosition: "replace",
+                carousel: {
+                    items: 3,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                imageSize: "&maxx=180&maxy=180",
+                maxImageHeight: 120,
+                showRatings: true,
+                wrapper: "<div class='PD24T'></div>",
+                rawJS: {
+                    preDisplay: function (tout) {
+                        $(tout.divSelect).hide();
+                    }
+                },
+                inCart: false
+            }],
+            Search: [{
+                enable: false
+            }],
+            Category: [{
+                enable: true,
+                resultType: 5,
+                numItems: 4,
+                caption: "More ideas",
+                productStyle: "product4T product4TCat",
+                divSelect: "#main4TellContainer",
+                divPosition: "append",
+                carousel: {
+                    items: 4,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                wrapper: "<div class='CAT4T'></div>",
+                rawJS: {},
+                inCart: false
+            }],
+            AddToCart: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "You may also like",
+                productStyle: "product4T product4TVC",
+                divSelect: ".chk-buttons",
+                divPosition: "below",
+                carousel: {
+                    items: 3,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                maxImageHeight: 100,
+                imageSize: "&maxx=180&maxy=180",
+                showRatings: true,
+                wrapper: "<div class='VC4T'></div>",
+                rawJS: {},
+                inCart: true
+            }]
+        }
+    }
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
