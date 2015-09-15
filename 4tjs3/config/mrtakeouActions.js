6514196af13da9944fc78943ff79f393884c6817
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:32:08 -0700

    live4tjs3mrtakeouActions.js - initial pull

---
+++ 4tjs3/config/mrtakeouActions.js
@@ -0,0 +1,7 @@
+﻿(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+
+    _4TellBoost.Service.address = "stage.4-Tell.net/Boost2.0/"
+
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
+//self-invoked namespace that protects $ and undefined internally
