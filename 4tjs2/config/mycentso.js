d1f2efeed22219266cf7d859bce9a84ac11d00db
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:34:18 -0700

    snapshot4tjs2mycentso.js - initial pull

--- 4tjs2/config/mycentso.js
+++ 4tjs2/config/mycentso.js
@@ -1,16 +1,14 @@
 (function (_4TellBoost, $, undefined) {
     _4TellBoost.CONFIG = {
         SiteInfo: {
-            baseURL: "www.mycentsofstyle.com/",
+            baseURL: "www.centsofstyle.com",
             alias: "mycentso",
             GA_UA: "UA-10001586-2",
             platform: "4TellShp.js",
             custom: true,
-            ABTesting: 2,
-            persistentTesting: false,
-            addCartBtnAtts: "<input type='submit' value='Item Details' class='add_to_cart'>",
-            pricePrefix: "",
-            salePricePrefix: "",
+            addCartBtnAtts: "<input type='submit' value='Add to Cart' class='add_to_cart'>",
+            pricePrefix: "Our Price: ",
+            salePricePrefix: "On Sale: ",
             includeBase: false,
             siteEnable: true
         },
@@ -19,7 +17,6 @@
                 enable: true,
                 resultType: 1,
                 numItems: 12,
-                fillmode: "all",
                 caption: "Recommended for you",
                 productStyle: "product4T product4THome",
                 divSelect: ".content .twelve.columns",
@@ -30,23 +27,8 @@
                 },
                 showRatings: true,
                 maxImageHeight: 150,
-                wrapper: "<div class='HOME4T'></div>",
-                rawJS: {
-                    preInit: function (tout) {
-                        if (window.outerWidth < 750) {
-                            tout.numItems = tout.carousel.numVis;
-                            tout.carousel = false;
-                        }
-                    }
-                },
-                testGroup: {
-                    0: {},
-                    1: {
-                        enable: false,
-                        rawJS: {}
-
-                    }
-                },
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
                 inCart: false
             }],
             ProductDetail: [{
@@ -64,23 +46,8 @@
                 showRatings: true,
                 maxImageHeight: 150,
                 wrapper: "<div class='PD14T'></div>",
-                rawJS: {
-                    preInit: function (tout) {
-                        if (window.outerWidth < 750) {
-                            tout.numItems = tout.carousel.numVis;
-                            tout.carousel = false;
-                        }
-                    }
-                },
-                inCart: false,
-                testGroup: {
-                    0: {},
-                    1: {
-                        enable: false,
-                        rawJS: {}
-
-                    }
-                }
+                rawJS: {},
+                inCart: false
             }, {
                 enable: false,
                 resultType: 3,
@@ -97,7 +64,7 @@
                 enable: false
             }],
             Category: [{
-                enable: false,
+                enable: true,
                 resultType: 5,
                 numItems: 12,
                 caption: "More ideas...",
@@ -117,64 +84,20 @@
             AddToCart: [{
                 enable: true,
                 resultType: 0,
-                numItems: 10,
-                caption: "You may also like",
+                numItems: 12,
+                caption: "You may also like...",
                 productStyle: "product4T product4TVC",
                 divSelect: "#cart_form > div",
                 divPosition: "below",
                 carousel: {
-                    numVis: 5,
+                    numVis: 4,
                     circular: true
                 },
                 maxImageHeight: 100,
                 showRatings: true,
                 wrapper: "<div class='VC4T'></div>",
-                rawJS: {
-                    preInit: function (tout) {
-                        _4TellBoost.UserData.clearCart()
-                        $(".CART4T").each(function () { _4TellBoost.addCartItem($(this).attr("ID")) });
-
-                        if (window.outerWidth < 750) {
-                            tout.numItems = 4;
-                            tout.carousel = false;
-                        }
-                    }
-                },
-                inCart: true,
-                testGroup: {
-                    0: {},
-                    1: {
-                        enable: false,
-                        rawJS: {}
-
-                    }
-                }
-            }],
-            QuickCart: [{
-                enable: true,
-                resultType: 0,
-                numItems: 6,
-                caption: "You may also like",
-                productStyle: "product4T product4TQC",
-                divSelect: "#cart > ul",
-                divPosition: "below",
-                carousel: {
-                    numVis: 2,
-                    circular: true
-                },
-                maxImageHeight: 100,
-                showRatings: true,
-                wrapper: "<div class='QC4T'></div>",
                 rawJS: {},
-                inCart: true,
-                testGroup: {
-                    0: {},
-                    1: {
-                        enable: false,
-                        rawJS: {}
-
-                    }
-                }
+                inCart: true
             }]
         }
     }
