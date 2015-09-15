06e8cb43659323281a89668d825e3cf2d5976b01
dougadams4 <dougadams4@gmail.com>
2015-09-15 09:17:28 -0700

    snapshot4tjs3SigmaMoz.js - initial pull

--- 4tjs3/config/SigmaMoz.js
+++ 4tjs3/config/SigmaMoz.js
@@ -1,1 +1,240 @@
-(function(n,t){n.CONFIG={SiteInfo:{baseURL:"www.CLIENTURL.com",alias:"SigmaMoz",GA_UA:"",platform:"4TellDIY.js",addCartBtnAtts:"button type='button'>Add To Bag<\/button",pricePrefix:"",salePricePrefix:"",includeBase:!1,siteEnable:!0},PageSettings:{Home:[{enable:!1}],ProductDetail:[{enable:!0,resultType:0,numItems:12,fillMode:"none",caption:"Our customers also bought...",showCaption:!1,productStyle:"product4T product4TPD1",divSelect:"#tout1_pdp_4Tell",divPosition:"replace",showRatings:!0,maxImageHeight:500,wrapper:"<div class='PD14T'><\/div>",rawJS:{perProduct:function(n){n.imageLink+="?max=300"}},inCart:!1,carousel:{items:4,pagination:!1,scrollPerPage:!0,itemsCustom:[[0,1],[480,2],[700,2],[1e3,3],[1025,4]],navigation:!0,rewindNav:!1}},{enable:!0,resultType:4,numItems:12,caption:"Similar Items",showCaption:!1,productStyle:"product4T product4TPD1",divSelect:"section[data-mz-tabs-id='TopSeller'] > div",divPosition:"append",maxImageHeight:500,showRatings:!0,wrapper:"<div class='PD14T'><\/div>",rawJS:{perProduct:function(n){n.imageLink+="?max=300"}},inCart:!1,carousel:{items:4,pagination:!1,scrollPerPage:!0,itemsCustom:[[0,1],[480,2],[700,2],[1e3,3],[1025,4]],navigation:!0,rewindNav:!1}}],Search:[{enable:!0,resultType:0,numItems:12,fillMode:"none",caption:"Our customers also bought...",showCaption:!1,productStyle:"product4T product4TPD1",divSelect:"#tout1_srch_4Tell",divPosition:"replace",showRatings:!0,maxImageHeight:500,wrapper:"<div class='PD14T'><\/div>",rawJS:{perProduct:function(n){n.imageLink+="?max=300"},preInit:function(i){n.addProductID(t(i.divSelect).data().products)}},inCart:!1,carousel:{items:4,pagination:!1,scrollPerPage:!0,itemsCustom:[[0,1],[480,2],[700,2],[1e3,3],[1025,4]],navigation:!0,rewindNav:!1}},{enable:!0,resultType:4,numItems:12,caption:"Similar Items",showCaption:!1,productStyle:"product4T product4TPD1",divSelect:"section[data-mz-tabs-id='TopSeller'] > div",divPosition:"append",maxImageHeight:500,showRatings:!0,wrapper:"<div class='PD14T'><\/div>",rawJS:{perProduct:function(n){n.imageLink+="?max=300"}},inCart:!1,carousel:{items:4,pagination:!1,scrollPerPage:!0,itemsCustom:[[0,1],[480,2],[700,2],[1e3,3],[1025,4]],navigation:!0,rewindNav:!1}}],Category:[{enable:!0,resultType:5,numItems:12,fillMode:"none",caption:"You may also like...",showCaption:!1,productStyle:"product4T product4TCat",divSelect:"#tout1_cat_4Tell",divPosition:"replace",maxImageHeight:500,showRatings:!0,wrapper:"<div class='PD14T'><\/div>",rawJS:{preInit:function(){n.setCatId(jQuery(".mz-breadcrumb-link").map(function(){return jQuery(this).attr("href").match(/\d+$/)}).toArray().join()+","+n.Service.catId)},perProduct:function(n){n.imageLink+="?max=300"}},inCart:!0,carousel:{items:4,pagination:!1,scrollPerPage:!0,itemsCustom:[[0,1],[480,2],[700,2],[1e3,3],[1025,4]],navigation:!0,rewindNav:!1}},{enable:!0,resultType:4,numItems:12,caption:"Similar Items",showCaption:!1,productStyle:"product4T product4TCat",divSelect:"section[data-mz-tabs-id='TopSeller'] > div",divPosition:"append",maxImageHeight:500,showRatings:!0,wrapper:"<div class='PD14T'><\/div>",rawJS:{perProduct:function(n){n.imageLink+="?max=300"}},inCart:!1,carousel:{items:4,pagination:!1,scrollPerPage:!0,itemsCustom:[[0,1],[480,2],[700,2],[1e3,3],[1025,4]],navigation:!0,rewindNav:!1}}],AddToCart:[{enable:!0,resultType:0,numItems:12,caption:"You may also like...",showCaption:!1,productStyle:"product4T product4TVC",divSelect:"#tout1_cart_4Tell",divPosition:"replace",maxImageHeight:500,showRatings:!0,wrapper:"<div class='PD14T'><\/div>",rawJS:{perProduct:function(n){n.imageLink+="?max=300"}},inCart:!0,carousel:{items:4,pagination:!1,scrollPerPage:!0,itemsCustom:[[0,1],[480,2],[700,2],[1e3,3],[1025,4]],navigation:!0,rewindNav:!1}},{enable:!0,resultType:4,numItems:12,caption:"Similar Items",showCaption:!1,productStyle:"product4T product4TVC",divSelect:"section[data-mz-tabs-id='TopSeller'] > div",divPosition:"append",maxImageHeight:500,showRatings:!0,wrapper:"<div class='PD14T'><\/div>",rawJS:{perProduct:function(n){n.imageLink+="?max=300"}},inCart:!1,carousel:{items:4,pagination:!1,scrollPerPage:!0,itemsCustom:[[0,1],[480,2],[700,2],[1e3,3],[1025,4]],navigation:!0,rewindNav:!1}}]}}})(window._4TellBoost=window._4TellBoost||{},jQuery);
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.CONFIG = {
+        SiteInfo: {
+            baseURL: "www.CLIENTURL.com",
+            alias: "SigmaMoz",
+            GA_UA: "",
+            platform: "4TellDIY.js",
+            addCartBtnAtts: "button type='button'>Add To Bag</button",
+            pricePrefix: "",
+            salePricePrefix: "",
+            includeBase: false,
+            siteEnable: true
+        },
+        PageSettings: {
+            Home: [{
+                enable: false
+            }],
+            ProductDetail: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                fillMode: "none",
+                caption: "Our customers also bought...",
+                showCaption: false,
+                productStyle: "product4T product4TPD1",
+                divSelect: "#tout1_pdp_4Tell",
+                divPosition: "replace",
+                showRatings: true,
+                maxImageHeight: 500,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink += "?max=300";
+                    }
+                },
+                inCart: false,
+                carousel: {
+                    items: 4,
+                    pagination: false,
+                    scrollPerPage: true,
+                    itemsCustom: [[0, 1], [480, 2], [700, 2], [1000, 3], [1025, 4]],
+                    navigation: true,
+                    rewindNav: false
+                }
+            }, {
+                enable: true,
+                resultType: 4,
+                numItems: 12,
+                caption: "Similar Items",
+                showCaption: false,
+                productStyle: "product4T product4TPD1",
+                divSelect: "section[data-mz-tabs-id='TopSeller'] > div",
+                divPosition: "append",
+                maxImageHeight: 500,
+                showRatings: true,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink += "?max=300";
+                    }
+                },
+                inCart: false,
+                carousel: {
+                    items: 4,
+                    pagination: false,
+                    scrollPerPage: true,
+                    itemsCustom: [[0, 1], [480, 2], [700, 2], [1000, 3], [1025, 4]],
+                    navigation: true,
+                    rewindNav: false
+                }
+            }],
+            Search: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                fillMode: "none",
+                caption: "Our customers also bought...",
+                showCaption: false,
+                productStyle: "product4T product4TPD1",
+                divSelect: "#tout1_srch_4Tell",
+                divPosition: "replace",
+                showRatings: true,
+                maxImageHeight: 500,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink += "?max=300";
+                    },
+                    preInit: function (tout) {
+                        _4TellBoost.addProductID($(tout.divSelect).data()["products"]);
+                    }
+                },
+                inCart: false,
+                carousel: {
+                    items: 4,
+                    pagination: false,
+                    scrollPerPage: true,
+                    itemsCustom: [[0, 1], [480, 2], [700, 2], [1000, 3], [1025, 4]],
+                    navigation: true,
+                    rewindNav: false
+                }
+            }, {
+                enable: true,
+                resultType: 4,
+                numItems: 12,
+                caption: "Similar Items",
+                showCaption: false,
+                productStyle: "product4T product4TPD1",
+                divSelect: "section[data-mz-tabs-id='TopSeller'] > div",
+                divPosition: "append",
+                maxImageHeight: 500,
+                showRatings: true,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink += "?max=300";
+                    }
+                },
+                inCart: false,
+                carousel: {
+                    items: 4,
+                    pagination: false,
+                    scrollPerPage: true,
+                    itemsCustom: [[0, 1], [480, 2], [700, 2], [1000, 3], [1025, 4]],
+                    navigation: true,
+                    rewindNav: false
+                }
+            }],
+            Category: [{
+                enable: true,
+                resultType: 5,
+                numItems: 12,
+                fillMode: "none",
+                caption: "You may also like...",
+                showCaption: false,
+                productStyle: "product4T product4TCat",
+                divSelect: "#tout1_cat_4Tell",
+                divPosition: "replace",
+                maxImageHeight: 500,
+                showRatings: true,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink += "?max=300";
+                    }
+                },
+                inCart: true,
+                carousel: {
+                    items: 4,
+                    pagination: false,
+                    scrollPerPage: true,
+                    itemsCustom: [[0, 1], [480, 2], [700, 2], [1000, 3], [1025, 4]],
+                    navigation: true,
+                    rewindNav: false
+                }
+            },
+             {
+                 enable: true,
+                 resultType: 4,
+                 numItems: 12,
+                 caption: "Similar Items",
+                 showCaption: false,
+                 productStyle: "product4T product4TCat",
+                 divSelect: "section[data-mz-tabs-id='TopSeller'] > div",
+                 divPosition: "append",
+                 maxImageHeight: 500,
+                 showRatings: true,
+                 wrapper: "<div class='PD14T'></div>",
+                 rawJS: {
+                     perProduct: function (itemdata) {
+                         itemdata.imageLink += "?max=300";
+                     }
+                 },
+                 inCart: false,
+                 carousel: {
+                     items: 4,
+                     pagination: false,
+                     scrollPerPage: true,
+                     itemsCustom: [[0, 1], [480, 2], [700, 2], [1000, 3], [1025, 4]],
+                     navigation: true,
+                     rewindNav: false
+                 }
+             }],
+            AddToCart: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "You may also like...",
+                showCaption: false,
+                productStyle: "product4T product4TVC",
+                divSelect: "#tout1_cart_4Tell",
+                divPosition: "replace",
+                maxImageHeight: 500,
+                showRatings: true,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {
+                    perProduct: function (itemdata) {
+                        itemdata.imageLink += "?max=300";
+                    }
+                },
+                inCart: true,
+                carousel: {
+                    items: 4,
+                    pagination: false,
+                    scrollPerPage: true,
+                    itemsCustom: [[0, 1], [480, 2], [700, 2], [1000, 3], [1025, 4]],
+                    navigation: true,
+                    rewindNav: false
+                }
+            },
+             {
+                 enable: true,
+                 resultType: 4,
+                 numItems: 12,
+                 caption: "Similar Items",
+                 showCaption: false,
+                 productStyle: "product4T product4TVC",
+                 divSelect: "section[data-mz-tabs-id='TopSeller'] > div",
+                 divPosition: "append",
+                 maxImageHeight: 500,
+                 showRatings: true,
+                 wrapper: "<div class='PD14T'></div>",
+                 rawJS: {
+                     perProduct: function (itemdata) {
+                         itemdata.imageLink += "?max=300";
+                     }
+                 },
+                 inCart: false,
+                 carousel: {
+                     items: 4,
+                     pagination: false,
+                     scrollPerPage: true,
+                     itemsCustom: [[0, 1], [480, 2], [700, 2], [1000, 3], [1025, 4]],
+                     navigation: true,
+                     rewindNav: false
+                 }
+             }]
+        }
+    }
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
