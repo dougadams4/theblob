f472d6b7a729a183e3bd4bafda1ad723c7da5212
dougadams4 <dougadams4@gmail.com>
2015-09-15 09:01:38 -0700

    snapshot4tjs4sassysec.js - initial pull

--- 4tjs4/config/sassysec.js
+++ 4tjs4/config/sassysec.js
@@ -5,71 +5,186 @@
             alias: "sassysec",
             GA_UA: "UA-6517893-2",
             platform: "4TellVs.js",
-            addCartBtnAtts: "img class='vCSS_input_addtocart' alt='Add to Cart'",
+            custom: true,
+            addCartBtnAtts: "input type='button' class='btn' value='View Product'",
             addCartImage: "/v/vspfiles/templates/SS10-9-14/images/buttons/btn_addtocart_small.gif",
             spacerImage: "/v/vspfiles/templates/SS10-9-14/images/clear1x1.gif",
             emptyImage: "/v/vspfiles/templates/SS10-9-14/images/nophoto.gif",
             pricePrefix: "Our Price: ",
             salePricePrefix: "On Sale: ",
             includeBase: false,
-            siteEnable: false
+            siteEnable: true
         },
         PageSettings: {
             Home: [{
                 enable: true,
                 resultType: 1,
                 numItems: 12,
-                caption: "Our customers also bought...",
-                showCaption: false,
+                caption: "Our customers also bought",
+                showCaption: true,
                 productStyle: "product4T product4THome",
-                divSelect: ".v65-productDisplay",
-                divPosition: "replace",
+                divSelect: ".cat-wrap",
+                divPosition: "below",
                 carousel: {
-                    numVis: 4,
-                    circular: true
+                    items: 4,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: ["",""],
+                    pagination: false
                 },
+                responsive: {
+                    1: {
+                        carousel: {
+                             items: 4,
+                        },
+                    },
+
+                    2: {
+                       carousel: {
+                             items: 3,
+                       },
+                    },
+
+                    3: {
+                        carousel: {
+                             items: 2,
+                        },
+                    },
+
+                    4: {
+                       carousel: {
+                             items: 1,
+                             },
+                    }
+                },
+
                 showRatings: true,
                 maxImageHeight: 150,
                 wrapper: "<div class='HOME4T'></div>",
-                rawJS: {},
+                rawJS: {
+                    perProduct: function(itemdata) {
+                         if(itemdata.title.length > 20) {
+                               itemdata.title = itemdata.title.substring(0,17) + "...";
+
+                         }
+                     },
+                     
+                    
+                },
                 inCart: false
             }],
             ProductDetail: [{
                 enable: true,
                 resultType: 0,
                 numItems: 6,
-                caption: "Customers also bought...",
+                caption: "Customers also bought",
                 productStyle: "product4T product4TPD1",
-                divSelect: ".colors_pricebox",
+                divSelect: "#v65-product-parent",
                 divPosition: "below",
                 carousel: {
-                    numVis: 3,
-                    circular: true
+                    items: 4,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                responsive: {
+                    1: {
+                        carousel: {
+                             items: 4,
+                        },
+                    },
+
+                    2: {
+                       carousel: {
+                             items: 3,
+                       },
+                    },
+
+                    3: {
+                        carousel: {
+                             items: 2,
+                        },
+                    },
+
+                    4: {
+                       carousel: {
+                             items: 1,
+                             },
+                    }
                 },
                 showRatings: true,
                 maxImageHeight: 150,
                 wrapper: "<div class='PD14T'></div>",
-                rawJS: {},
+                rawJS: {
+                         perProduct: function(itemdata) {
+                           if(itemdata.title.length > 15) {
+                                   itemdata.title = itemdata.title.substring(0,12) + "...";
+
+                           }
+                         }
+                       },
                 inCart: false
             }, {
                 enable: true,
                 resultType: 3,
                 numItems: 12,
-                caption: "Customers also bought...",
+                caption: "Customers also viewed",
                 productStyle: "product4T product4TPD2",
                 divSelect: ".colors_lines_light",
                 divPosition: "replace",
+                navigation: true,
                 carousel: {
-                    numVis: 4,
-                    circular: true
+                    items: 4,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                responsive: {
+                    1: {
+                        carousel: {
+                             items: 4,
+                        },
+                    },
+
+                    2: {
+                       carousel: {
+                             items: 3,
+                       },
+                    },
+
+                    3: {
+                        carousel: {
+                             items: 2,
+                        },
+                    },
+
+                    4: {
+                       carousel: {
+                             items: 1,
+                             },
+                    }
                 },
                 maxImageHeight: 120,
                 showRatings: true,
                 wrapper: "<div class='PD24T'></div>",
                 rawJS: {
-                    preDisplay: function (tout) {
+                    preInit: function (tout) {
                         $(tout.divSelect).hide();
-                    }
+                    },
+                     
+                    preDisplay: function (tout) {
+                        $(tout.divSelect).parents("table:last").css("table-layout", "fixed");
+                    },
+                    
+                    perProduct: function(itemdata) {
+                         if(itemdata.title.length > 20) {
+                               itemdata.title = itemdata.title.substring(0,17) + "...";
+
+                         }
+                     }
+
                 },
                 inCart: false
             }],
@@ -96,23 +211,34 @@
             AddToCart: [{
                 enable: true,
                 resultType: 0,
-                numItems: 6,
+                numItems: 3,
                 caption: "You may also like...",
                 captionStyle: "product4TCaption product4TCaptionVC",
                 productStyle: "product4T product4TVC",
-                divSelect: "table#v65-cart-checkout-parent td:first",
-                divPosition: "replace",
+                divSelect: ".btn_checkout_guest",
+                divPosition: "below",
                 carousel: {
-                    numVis: 2,
-                    circular: true
+                    items: 3,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
                 },
+                
                 maxImageHeight: 100,
                 showRatings: true,
                 wrapper: "<td valign='top'><div class='VC4T'></div></td>",
                 rawJS: {
-                    preDisplay: function () {
-                        $("table:has(.v65-productDisplay):last").hide();
-                    }
+                    
+                    perProduct: function(itemdata) {
+                         if(itemdata.title.length > 20) {
+                               itemdata.title = itemdata.title.substring(0,17) + "...";
+
+                         }
+                     }
+
+                    
+
                 },
                 inCart: true
             }]
