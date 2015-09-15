7e7e501df7078281ce5c5846104f90a0a61d8b8d
dougadams4 <dougadams4@gmail.com>
2015-09-15 10:06:30 -0700

    live4tjs3usoutdrs.js - initial pull

---
+++ 4tjs3/config/usoutdrs.js
@@ -0,0 +1,57 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.CONFIG = {
+        SiteInfo: {
+            baseURL: "www.usoutdoor.com",
+            alias: "usoutdrs",
+            GA_UA: "UA-24464686-1",
+            platform: "4TellDIY.js",
+            addCartBtnAtts: "input type='image'",
+            addCartImage: "//az416957.vo.msecnd.net/4tjs/images/view.png",
+            priceClass: "price2",
+            pricePrefix: "",
+            salePricePrefix: "",
+            includeBase: false,
+            siteEnable: true
+        },
+        PageSettings: {
+            Home: {
+                tout1: {
+                    enable: false
+                }
+            },
+            ProductDetail: {
+                tout1: {
+                    enable: false
+                },
+                tout2: {
+                    enable: false
+                }
+            },
+            Category: {
+                tout1: {
+                    enable: false
+                }
+            },
+            Search: {
+                tout1: {
+                    enable: false
+                }
+            },
+            AddToCart: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 4,
+                    caption: "You may also like...",
+                    productStyle: "product4T product4TVC",
+                    divSelect: "#tout1_cart_4Tell > div",
+                    divPosition: "below",
+                    maxImageHeight: 100,
+                    showRatings: false,
+                    wrapper: "<div style='clear:left; float:left;'></div>",
+                    inCart: true
+                }
+            }
+        }
+    }
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
