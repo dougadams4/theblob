2e0c64d241902babd167458e430a59df8c46de1e
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:57:05 -0700

    snapshot4tjsRomanSun.js - initial pull

--- 4tjs/config/RomanSun.js
+++ 4tjs/config/RomanSun.js
@@ -16,30 +16,32 @@
             siteEnable: true
         },
         PageSettings: {
-            Home: [{
-                enable: false
-
-            }],
-            ProductDetail: [{
-                enable: true,
-                resultType: 0,
-                numItems: 3,
-                caption: "Our customers also bought...",
-                captionStyle: "titles product4TCaption",
-                productStyle: "product4T product4TPD1",
-                divSelect: "table#v65-product-parent",
-                divPosition: "below",
-                showRatings: true,
-                maxImageHeight: 150,
-                wrapper: "<table width='20%' align='right' class='PD14T'><tr><td></td></tr></table>",
-                rawJS: {
-                    preDisplay: function (tout) {
-                        $(tout.divSelect).attr({ "width": "80%", 'align': 'left' });
-                    }
-                },
-                inCart: false
+            Home: {
+                tout1: {
+                    enable: false
+                }
             },
-                {
+            ProductDetail: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 3,
+                    caption: "Our customers also bought...",
+                    captionStyle: "titles product4TCaption",
+                    productStyle: "product4T product4TPD1",
+                    divSelect: "table#v65-product-parent",
+                    divPosition: "below",
+                    showRatings: true,
+                    maxImageHeight: 150,
+                    wrapper: "<table width='20%' align='right' class='PD14T'><tr><td></td></tr></table>",
+                    rawJS: {
+                        preDisplay: function (tout) {
+                            $(tout.divSelect).attr({ "width": "80%", 'align': 'left' });
+                        }
+                    },
+                    inCart: false
+                },
+                tout2: {
                     enable: true,
                     resultType: 0,
                     numItems: 15,
@@ -56,37 +58,43 @@
                     maxImageHeight: 180,
                     wrapper: "<table width='100%' align='right' class='PD24T'><tr><td></td></tr></table>",
                     inCart: false
-
-                }],
-            Category: [{
-                enable: false
-            }],
-            Search: [{
-                enable: false
-            }],
-            AddToCart: [{
-                enable: true,
-                resultType: 0,
-                numItems: 6,
-                caption: "You may also like...",
-                captionStyle: "checkout-headers product4TCaption product4TCaptionVC",
-                productStyle: "product4T product4TVC",
-                divSelect: "tr.v65-cart-shipping-details-row td.v65-cart-details-firstcell",
-                divPosition: "replace",
-                maxImageHeight: 115,
-                showRatings: true,
-                carousel: {
-                    numVis: 2,
-                    circular: true
-                },
-                wrapper: "<td colspan='4' style='border: 1px solid #DFE0E0;'></td>",
-                rawJS: {
-                    preDisplay: function (tout) {
-                        $('table:has(font.colors_productname):last, #v65-cart-shipping-details-text').remove();
-                    }
-                },
-                inCart: true
-            }]
+                }
+            },
+            Category: {
+                tout1: {
+                    enable: false
+                }
+            },
+            Search: {
+                tout1: {
+                    enable: false
+                }
+            },
+            AddToCart: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 6,
+                    caption: "You may also like...",
+                    captionStyle: "checkout-headers product4TCaption product4TCaptionVC",
+                    productStyle: "product4T product4TVC",
+                    divSelect: "tr.v65-cart-shipping-details-row td.v65-cart-details-firstcell",
+                    divPosition: "replace",
+                    maxImageHeight: 115,
+                    showRatings: true,
+                    carousel: {
+                        numVis: 2,
+                        circular: true
+                    },
+                    wrapper: "<td colspan='4' style='border: 1px solid #DFE0E0;'></td>",
+                    rawJS: {
+                        preDisplay: function (tout) {
+                            $('table:has(font.colors_productname):last, #v65-cart-shipping-details-text').remove();
+                        }
+                    },
+                    inCart: true
+                }
+            }
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
