78b7888130a0263dc4e1681a8ddcb497f9de893f
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:34:27 -0700

    snapshot4tjs2mycentsoActions.js - initial pull

--- 4tjs2/config/mycentsoActions.js
+++ 4tjs2/config/mycentsoActions.js
@@ -1,19 +1,7 @@
 (function (_4TellBoost, $, undefined) {
     _4TellBoost.Service.customLoaded = true;
-    //_4TellBoost.Service.address = "stage.4-Tell.net/Boost2.0/";
-    _4TellBoost.getBuyBtn = function (tout, itemdata) {
-        var buyWrapper = $("<div class='productBuy' />");
-        var buyBtn = $(_4TellBoost.SiteInfo.addCartBtnAtts);
-        if (buyBtn)
-            buyBtn.appendTo(buyWrapper);
-        var newAddress = itemdata.pageLink;
+    _4TellBoost.Service.address = "stage.4-Tell.net/Boost2.0/";
 
-        //can't wrap with a-href in IE so add new address to the onclick handler
-        buyBtn.click(function () {
-            _4TellBoost.TrackClick(tout.toutType, itemdata.productID, newAddress);
-        });
-        return buyWrapper;
-    };
     $(document).ready(function () {
         var CATID = ($("#tout1_cat_4Tell").length && $("#tout1_cat_4Tell").data().category)
         if (CATID) {
@@ -23,7 +11,7 @@
 
         $("input#add-to-cart, a.cart-button").click(function () {
             _4TellBoost.Service.called = false;
-            _4TellBoost.setPageType("QuickCart");
+            _4TellBoost.setPageType("AddToCart");
             _4TellBoost.Service.lastDivIDs = [];
             _4TellBoost.Touts = [];
             _4TellBoost.getRecommendations("Auto", "");
