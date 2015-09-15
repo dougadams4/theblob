49216b34c1ed97b418d43f5bf1c58f79600b6e5c
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:15:12 -0700

    live4tjs4luckysco.js - initial pull

---
+++ 4tjs4/config/luckysco.js
@@ -0,0 +1,116 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.CONFIG = {
+        SiteInfo: {
+            baseURL: "www.luckyscooters.com",
+            alias: "luckysco",
+            GA_UA: "UA-1513737-22",
+            platform: "4TellShp.js",
+            addCartBtnAtts: "<input type='submit' value='Item Details' class='btn add-to-cart'>",
+            pricePrefix: "Our Price: ",
+            salePricePrefix: "On Sale: ",
+            includeBase: false,
+            siteEnable: false
+        },
+        PageSettings: {
+            Home: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "Our customers also bought...",
+                productStyle: "product4T product4TPD1",
+                divSelect: "div.home_products",
+                divPosition: "replace",
+                carousel: {
+                    items: 4,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                showRatings: true,
+                maxImageHeight: 150,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
+                inCart: false
+            }],
+            ProductDetail: [{
+                enable: true,
+                resultType: 0,
+                numItems: 6,
+                caption: "Our customers also bought...",
+                productStyle: "product4T product4TPD1",
+                divSelect: ".sharing ",
+                divPosition: "below",
+                carousel: {
+                    items: 3,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                showRatings: true,
+                maxImageHeight: 150,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
+                inCart: false
+            }, {
+                enable: true,
+                resultType: 3,
+                numItems: 4,
+                caption: "Similar Items",
+                productStyle: "product4T product4TPD2",
+                divSelect: ".rel-container",
+                divPosition: "replace",
+                carousel: {
+                    items: 4,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                maxImageHeight: 120,
+                showRatings: true,
+                wrapper: "<div class='PD24T'></div>",
+                rawJS: {},
+                inCart: false
+            }],
+            Search: [{
+                enable: false
+            }],
+            Category: [{
+                enable: false,
+                resultType: 5,
+                numItems: 4,
+                caption: "More ideas...",
+                productStyle: "product4T product4TCat",
+                divSelect: "#main4TellContainer",
+                divPosition: "append",
+                setHeight: false,
+                setWidth: false,
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                wrapper: "<div class='CAT4T'></div>",
+                rawJS: {},
+                inCart: false
+            }],
+            AddToCart: [{
+                enable: true,
+                resultType: 0,
+                numItems: 16,
+                caption: "You may also like...",
+                productStyle: "product4T product4TVC",
+                divSelect: "#checkout-addnote",
+                divPosition: "below",
+                carousel: {
+                    items: 4,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                maxImageHeight: 100,
+                showRatings: true,
+                wrapper: "<div class='VC4T'></div>",
+                rawJS: {},
+                inCart: true
+            }]
+        }
+    }
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
