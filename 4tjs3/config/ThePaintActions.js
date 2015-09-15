59254bff6725c960dab0d19e6bbf8350012c3940
dougadams4 <dougadams4@gmail.com>
2015-09-15 09:49:24 -0700

    live4tjs3ThePaintActions.js - initial pull

---
+++ 4tjs3/config/ThePaintActions.js
@@ -0,0 +1,21 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+
+    _4TellBoost.getBuyBtn = function (tout, itemdata) {
+        var buyWrapper = $("<div class='productBuy' />");
+        var buyBtn = $("<" + _4TellBoost.SiteInfo.addCartBtnAtts + " />");
+
+        if (buyBtn)
+            buyBtn.appendTo(buyWrapper);
+        var newAddress = '';
+        if (_4TellBoost.SiteInfo.includeBase)
+            newAddress += 'http://' + _4TellBoost.SiteInfo.baseURL; //link to PDP should not be https
+        newAddress += itemdata.pageLink;
+
+        //can't wrap with a-href in IE so add new address to the onclick handler
+        buyBtn.click(function () {
+            _4TellBoost.TrackClick(tout.toutType, itemdata.productID, newAddress);
+        });
+        return buyWrapper;
+    };
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
