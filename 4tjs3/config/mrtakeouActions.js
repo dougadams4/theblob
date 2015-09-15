9e34f5387af17689bd985e32756e209146dd9042
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:32:26 -0700

    snapshot4tjs3mrtakeouActions.js - initial pull

--- 4tjs3/config/mrtakeouActions.js
+++ 4tjs3/config/mrtakeouActions.js
@@ -3,5 +3,25 @@
 
     _4TellBoost.Service.address = "stage.4-Tell.net/Boost2.0/"
 
+    _4TellBoost.getBuyBtn = function (tout, itemdata) {
+        var buyWrapper = $("<div class='productBuy' />");
+        var buyBtn = $("<" + _4TellBoost.SiteInfo.addCartBtnAtts + " />");
+        if (_4TellBoost.SiteInfo.addCartImage) {
+            var buyBtnImage = '';
+            if (_4TellBoost.SiteInfo.includeBase)
+                buyBtnImage = '//' + _4TellBoost.SiteInfo.baseURL + '/';
+            buyBtnImage += _4TellBoost.SiteInfo.addCartImage;
+            buyBtn.attr("src", buyBtnImage);
+        }
+        if (buyBtn)
+            buyBtn.appendTo(buyWrapper);
+        var newAddress = itemdata.pageLink;
+
+        //can't wrap with a-href in IE so add new address to the onclick handler
+        buyBtn.click(function () {
+            _4TellBoost.TrackGA(0, _4TellBoost.Service.pageType, itemdata.resultType, itemdata.title, 0, newAddress);
+        });
+        return buyWrapper;
+    };
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
 //self-invoked namespace that protects $ and undefined internally
