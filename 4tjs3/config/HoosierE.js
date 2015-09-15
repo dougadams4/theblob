7adee5b5a736cae9dc1067f8a84a98b7201e442f
dougadams4 <dougadams4@gmail.com>
2015-09-15 07:23:05 -0700

    snapshot4tjs3HoosierE.js - initial pull

--- 4tjs3/config/HoosierE.js
+++ 4tjs3/config/HoosierE.js
@@ -16,120 +16,116 @@
             siteEnable: true
         },
         PageSettings: {
-            Home: [{
-                enable: true,
-                resultType: 0,
-                numItems: 12,
-                caption: "Top Sellers",
-                captionStyle: "menu-headers product4TCaption product4TCaptionPD1",
-                productStyle: "product4T product4THome",
-                divSelect: "#modTopSellers",
-                divPosition: "replace",
-                imageSize: "&maxx=120&maxy=0",
-                showRatings: true,
-                maxImageHeight: 150,
-                wrapper: "<div class='CAT4T'></div>",
-                rawJS: {
-                    //preDisplay: function (tout) {
-                    //    $(tout.divSelect).hide();
-                    //    jQuery(".header-specials").text(tout.caption);
-                    //}
-                },
-                inCart: false
-
-            }],
-            ProductDetail: [{
-                enable: true,
-                resultType: 0,
-                numItems: 12,
-                caption: "Customers also bought",
-                captionStyle: "product4TCaption",
-                productStyle: "product4T product4TPD1",
-                divSelect: "#rTabs",
-                divPosition: "above",
-                carousel: {
-                    items: 4,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
-                },
-                showRatings: true,
-                maxImageHeight: 150,
-                imageSize: "&maxx=120&maxy=0",
-                wrapper: "<div class='PD14T'></div>",
-                inCart: false
+            Home: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 12,
+                    caption: "Top Sellers",
+                    captionStyle: "titles product4TCaption",
+                    productStyle: "product4T product4THome",
+                    divSelect: "tr:has(td.titles):last",
+                    divPosition: "above",
+                    carousel: {
+                        numVis: 4,
+                        circular: true
+                    },
+                    showRatings: true,
+                    maxImageHeight: 150,
+                    imageSize: "&maxx=120&maxy=120",
+                    wrapper: "<tr><td ></td></tr>",
+                    rawJS: { preDisplay: function () { $("#TOP_SELLERS_BLOCK").hide(); } },
+                    inCart: false
+                }
             },
-                {
+            ProductDetail: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 4,
+                    caption: "Customers also bought",
+                    captionStyle: "menu-headers product4TCaption product4TCaptionPD1",
+                    productStyle: "product4T product4TPD1",
+                    divSelect: "#column2 > *:first",
+                    divPosition: "replace",
+                    showRatings: true,
+                    maxImageHeight: 150,
+                    imageSize: "&maxx=120&maxy=0",
+                    wrapper: "<table width='195' class='PD14T'><tr><td></td></tr></table>",
+                    inCart: false
+                },
+                tout2: {
                     enable: true,
                     resultType: 3,
                     numItems: 12,
                     caption: "Customers also viewed...",
-                    captionStyle: "product4TCaption",
+                    captionStyle: "titles product4TCaption",
                     productStyle: "product4T product4TPD2",
-                    divSelect: ".relatedBlock",
-                    divPosition: "replace",
+                    divSelect: "#REV4T",
+                    divPosition: "above",
                     carousel: {
-                        items: 3,
-                        scrollPerPage: true,
-                        navigation: true,
-                        navigationText: false,
-                        pagination: false
+                        numVis: 3,
+                        circular: true
                     },
                     imageSize: "&maxx=120&maxy=120",
                     maxImageHeight: 120,
                     showRatings: true,
-                    wrapper: "<div class='PD14T'></div>",
+                    wrapper: "<tr><td colspan='2'><table style='margin: 0 auto'><tr><td></td></tr></table></td></tr>",
                     rawJS: {
-                        //preInit: function (tout) {
-                        //    //if (navigator.userAgent.match(/MSIE\s*8/)) tout.carousel = false;
-                        //},
-                        preDisplay: function (tout) {
-                            jQuery(tout.divSelect).hide();
+                        preInit: function (tout) {
+                            if (navigator.userAgent.match(/MSIE\s*8/)) tout.carousel = false;
+                        },
+                        preDisplay: function () {
+                            $("#REL4T").hide();
                         }
                     },
                     inCart: false
-                }],
-            Search: [{
-                enable: false
-            }],
-            Category: [{
-                enable: true,
-                resultType: 5,
-                numItems: 4,
-                caption: "Related Top Sellers",
-                captionStyle: "menu-headers product4TCaption product4TCaptionPD1",
-                productStyle: "product4T product4TCat",
-                divSelect: "#modTopSellers",
-                divPosition: "replace",
-                imageSize: "&maxx=120&maxy=0",
-                maxImageHeight: 100,
-                showRatings: true,
-                wrapper: "<div class='CAT4T'></div>",
-                inCart: false
-            }],
-            AddToCart: [{
-                enable: true,
-                resultType: 0,
-                numItems: 12,
-                caption: "You may also like...",
-                captionStyle: "titles product4TCaption",
-                productStyle: "product4T product4TVC",
-                divSelect: "#recalculate",
-                divPosition: "below",
-                carousel: {
-                    items: 4,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
-                },
-                imageSize: "&maxx=100&maxy=100",
-                maxImageHeight: 100,
-                showRatings: true,
-                wrapper: "<div class='PD14T'></div>",
-                inCart: true
-            }]
+                }
+            },
+            Search: {
+                tout1: {
+                    enable: false
+                }
+            },
+            Category: {
+                tout1: {
+                    enable: true,
+                    resultType: 5,
+                    numItems: 4,
+                    caption: "Related Top Sellers",
+                    captionStyle: "menu-headers product4TCaption",
+                    captionStyle: "menu-headers product4TCaption product4TCaptionPD1",
+                    productStyle: "product4T product4TCat",
+                    divSelect: "#column2 > *:first",
+                    divPosition: "replace",
+                    imageSize: "&maxx=120&maxy=0",
+                    maxImageHeight: 100,
+                    showRatings: true,
+                    wrapper: "<table width='195' class='PD14T'><tr><td></td></tr></table>",
+                    inCart: false
+                }
+            },
+            AddToCart: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 12,
+                    caption: "You may also like...",
+                    captionStyle: "titles product4TCaption",
+                    productStyle: "product4T product4TVC",
+                    divSelect: "tr:has(#Table5)",
+                    divPosition: "below",
+                    carousel: {
+                        numVis: 4,
+                        circular: true
+                    },
+                    imageSize: "&maxx=100&maxy=100",
+                    maxImageHeight: 100,
+                    showRatings: true,
+                    wrapper: "<tr><td colspan='6'></td></tr>",
+                    inCart: true
+                }
+            }
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
