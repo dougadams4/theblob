0767b91cf68c76355278a12d59f56ce833fac925
dougadams4 <dougadams4@gmail.com>
2015-09-15 01:04:41 -0700

    snapshot4tjs3Bestinso.js - initial pull

--- 4tjs3/config/Bestinso.js
+++ 4tjs3/config/Bestinso.js
@@ -9,169 +9,200 @@
             addCartBtnAtts: "input type='button' value='SEE DETAILS'  class=\"btn\" onmouseover=\"this.className='btn_over'\" onmouseout=\"this.className='btn'\"",
             spacerImage: "/web/assets/images/spacer.gif",
             emptyImage: "thumbnail.asp?file=/assets/images/default.jpg",
-            pricePrefix: "",
-            salePricePrefix: "Sale: ",
+            priceClass: "price2",
+            pricePrefix: "Our Price: ",
+            salePricePrefix: "On Sale: ",
             includeBase: false,
             siteEnable: true
         },
         PageSettings: {
-            Home: [{
-                enable: true,
-                resultType: 1,
-                numItems: 12,
-                caption: "Recommended for you",
-                productStyle: "product4T product4THome",
-                divSelect: "#homeCarousel",
-                divPosition: "below",
-                carousel: {
-                    items: 4,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
+            Home: {
+                tout1: {
+                    enable: true,
+                    resultType: "1,4",
+                    numItems: "6,6",
+                    caption: "Top Sellers",
+                    productStyle: "product4T product4THome",
+                    divSelect: "#Table_01",
+                    divPosition: "replace",
+                    carousel: {
+                        numVis: 3,
+                        circular: true
+                    },
+                    showRatings: true,
+                    showBuyButton: false,
+                    imageSize: "&maxx=130&maxy=130",
+                    maxImageHeight: 180,
+                    wrapper: "<table width='100%' class='PD14T'><tr><td></td></tr></table>",
+                    inCart: false
                 },
-                imageSize: "&maxx=130&maxy=130",
-                maxImageHeight: 180,
-                wrapper: "<div class='HOME4T'></div>",
-                rawJS: {},
-                inCart: false
-            }],
-            ProductDetail: [{
-                enable: true,
-                resultType: 0,
-                numItems: 6,
-                caption: "Customers also bought",
-                //showCaption: false,
-                productStyle: "product4T product4TPD1",
-                divSelect: ".addl-images, .main-image",
-                divPosition: "below",
-                carousel: {
-                    items: 2,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
+                tout2: {
+                    enable: true,
+                    resultType: 4,
+                    numItems: 3,
+                    caption: "Customers also viewed...",
+                    showCaption: false,
+                    productStyle: "product4T product4TPD1",
+                    divSelect: ".hotprod  tr:last",
+                    divPosition: "replace",
+                    imageSize: "&maxx=120&maxy=120",
+                    maxImageHeight: 220,
+                    showRatings: true,
+                    showBuyButton: false,
+                    wrapper: "<tr class='PD14T'><td></td></tr>",
+                    inCart: false
+                }
+            },
+            ProductDetail: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 3,
+                    caption: "Customers also bought...",
+                    showCaption: false,
+                    productStyle: "product4T product4TPD1",
+                    divSelect: ".hotprod tr:last",
+                    divPosition: "replace",
+                    showRatings: true,
+                    showBuyButton: false,
+                    imageSize: "&maxx=120&maxy=120",
+                    maxImageHeight: 160,
+                    wrapper: "<tr class='PD14T'><td></td></tr>",
+                    inCart: false
                 },
-                imageSize: "&maxx=120&maxy=120",
-                maxImageHeight: 160,
-                wrapper: "<div class='PD14T'></div>",
-                rawJS: {},
-                inCart: false
-            }, {
-                enable: true,
-                resultType: 3,
-                numItems: 12,
-                caption: "Customers also viewed",
-                productStyle: "product4T product4TPD2",
-                divSelect: ".relatedBlock, .accessoriesBlock",
-                divPosition: "replace",
-                carousel: {
-                    items: 4,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
+                tout2: {
+                    enable: true,
+                    resultType: 3,
+                    numItems: 6,
+                    caption: "Customers also viewed...",
+                    productStyle: "product4T product4TPD2",
+                    divSelect: "#REL4T a[name=Review]",
+                    divPosition: "above",
+                    carousel: {
+                        numVis: 3,
+                        circular: true
+                    },
+                    imageSize: "&maxx=120&maxy=120",
+                    maxImageHeight: 220,
+                    showRatings: true,
+                    showBuyButton: false,
+                    wrapper: "<tr><td><div class='PD24T'></div></td></tr>",
+                    rawJS: {
+                        preDisplay: function (tout) {
+                            $(tout.divSelect).prevAll("div").hide();
+                        }
+                    },
+                    inCart: false
+                }
+            },
+            Search: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 3,
+                    caption: "Customers also bought...",
+                    showCaption: false,
+                    productStyle: "product4T product4TPD1",
+                    divSelect: ".hotprod tr:last",
+                    divPosition: "replace",
+                    showRatings: true,
+                    showBuyButton: false,
+                    imageSize: "&maxx=120&maxy=120",
+                    maxImageHeight: 160,
+                    wrapper: "<tr class='PD14T'><td></td></tr>",
+                    inCart: false
+                }
+            },
+            Category: {
+                tout1: {
+                    enable: true,
+                    resultType: 5,
+                    numItems: 12,
+                    caption: "Related Top Sellers",
+                    productStyle: "product4T product4TCat",
+                    divSelect: "#Table3",
+                    divPosition: "above",
+                    carousel: {
+                        numVis: 3,
+                        circular: true
+                    },
+                    imageSize: "&maxx=100&maxy=100",
+                    maxImageHeight: 100,
+                    showRatings: true,
+                    showBuyButton: false,
+                    wrapper: "<div id='CAT4T'></div>",
+                    rawJS: {
+                        preInit: function (tout) {
+                            tout.enable = tout.enable && $("table.alternative").length >= 20;
+                            if ($("#info-pain-div").length) {
+                                tout.divPosition = "below";
+                                tout.enable = true;
+                                tout.divSelect = "td.data > table";
+                            }
+                        }
+                    },
+                    inCart: false
                 },
-                imageSize: "&maxx=120&maxy=120",
-                maxImageHeight: 220,
-                wrapper: "<div class='PD14T'></div>",
-                rawJS: {
-                    preDisplay: function (tout) {
-                        $(tout.divSelect).hide();
-                    }
+                tout2: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 3,
+                    caption: "Customers also viewed...",
+                    showCaption: false,
+                    productStyle: "product4T product4TPD1",
+                    divSelect: ".hotprod  tr:last",
+                    divPosition: "replace",
+                    imageSize: "&maxx=120&maxy=120",
+                    maxImageHeight: 220,
+                    showRatings: true,
+                    showBuyButton: false,
+                    wrapper: "<tr class='PD14T'><td></td></tr>",
+                    inCart: false
+                }
+            },
+            AddToCart: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 12,
+                    caption: "You may also like...",
+                    productStyle: "product4T product4TVC",
+                    divSelect: "tr:has(img[src*=Amazon]):last td:last",
+                    divPosition: "above",
+                    carousel: {
+                        numVis: 4,
+                        circular: true
+                    },
+                    imageSize: "&maxx=100&maxy=100",
+                    maxImageHeight: 180,
+                    showRatings: true,
+                    showBuyButton: false,
+                    wrapper: "<td colspan='6'><div class='VC4T'></div></td>",
+                    rawJS: {
+                        preDisplay: function (tout) {
+                            $(tout.divSelect).siblings(":empty").remove();
+                            $(tout.divSelect).attr("valign", "top");
+                        }
+                    },
+                    inCart: true
                 },
-                inCart: false
-            }],
-            Search: [{
-                enable: false,
-                resultType: 0,
-                numItems: 3,
-                caption: "Customers also bought...",
-                showCaption: false,
-                productStyle: "product4T product4TPD1",
-                divSelect: ".hotprod tr:last",
-                divPosition: "replace",
-                imageSize: "&maxx=120&maxy=120",
-                maxImageHeight: 160,
-                wrapper: "<tr class='PD14T'><td></td></tr>",
-                rawJS: {
-                    perProduct: function (itemdata) {
-                        itemdata.imageLink = itemdata.imageLink.replace(/^www.bestinsoles.com/, "");
-                    }
-                },
-                inCart: false
-            }],
-            Category: [{
-                enable: true,
-                resultType: 5,
-                numItems: 12,
-                caption: "Related Top Sellers",
-                productStyle: "product4T product4TCat",
-                divSelect: "#categoryHeader",
-                divPosition: "replace",
-                carousel: {
-                    items: 4,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
-                },
-                imageSize: "&maxx=100&maxy=100",
-                maxImageHeight: 180,
-                wrapper: "<div class='CAT4T'></div>",
-                rawJS: {
-                    preDisplay: function (tout) {
-                        jQuery("#categoryHeader").hide();
-                        jQuery(tout.divSelect).css("float", "left");
-                    }
-                },
-                inCart: true
-            }],
-            AddToCart: [{
-                enable: true,
-                resultType: 0,
-                numItems: 12,
-                caption: "You may also like...",
-                showCaption: false,
-                productStyle: "product4T product4TVC",
-                divSelect: ".productBlockContainer",
-                divPosition: "replace",
-                carousel: {
-                    items: 4,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
-                },
-                imageSize: "&maxx=100&maxy=100",
-                maxImageHeight: 180,
-                wrapper: "<div class='VC4T'></div>",
-                rawJS: {
-                    preDisplay: function (tout) {
-                        //$(tout.divSelect).prev(".header").text(tout.caption);
-                    }
-                },
-                inCart: true
-            }],
-            Auto: [{
-                enable: true,
-                resultType: 5,
-                numItems: 4,
-                catId: "100",
-                caption: "You may also like...",
-                productStyle: "product4T product4TAuto",
-                divSelect: "#pain-container, #Table_01",
-                divPosition: "below",
-                imageSize: "&maxx=100&maxy=100",
-                maxImageHeight: 180,
-                wrapper: "<div class='AUTO4T'></div>",
-                rawJS: {
-                    preDisplay: function (tout) {
-                        jQuery(tout.divSelect).css("float", "left");
-                    }
-                },
-                inCart: true
-            }]
+                tout2: {
+                    enable: true,
+                    resultType: 4,
+                    numItems: 3,
+                    caption: "Customers also viewed...",
+                    showCaption: false,
+                    productStyle: "product4T product4TPD1",
+                    divSelect: ".hotprod  tr:last",
+                    divPosition: "replace",
+                    imageSize: "&maxx=120&maxy=120",
+                    maxImageHeight: 220,
+                    showRatings: true,
+                    showBuyButton: false,
+                    wrapper: "<tr class='PD14T'><td></td></tr>",
+                    inCart: false
+                }
+            }
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
