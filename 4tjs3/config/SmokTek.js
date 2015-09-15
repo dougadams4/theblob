b0ae58c3f7ea20b5638770891fbf4179bbc21f4f
dougadams4 <dougadams4@gmail.com>
2015-09-15 09:22:50 -0700

    snapshot4tjs3SmokTek.js - initial pull

--- 4tjs3/config/SmokTek.js
+++ 4tjs3/config/SmokTek.js
@@ -3,191 +3,121 @@
         SiteInfo: {
             baseURL: "www.SmokTek.com",
             alias: "SmokTek",
-            GA_UA: "UA-56656525-1",
+            GA_UA: "UA-24321404-1",
             platform: "4Tell3d.js",
             addCartBtnAtts: "input type='button' value='Add to Cart' class='btn' onmouseout=this.className='btn' onmouseover=this.className='btn_over'",
             addCartImage: "",
             spacerImage: "/web/assets/images/spacer.gif",
             emptyImage: "thumbnail.asp?file=/assets/images/default.jpg",
-            pricePrefix: "",
-            salePricePrefix: "",
+            priceClass: "price2",
+            pricePrefix: "Our Price: ",
+            salePricePrefix: "On Sale: ",
             includeBase: false,
             siteEnable: true
         },
         PageSettings: {
-            Home: [{
-                enable: true,
-                resultType: 4,
-                numItems: 4,
-                rotateRecs: true,
-                caption: "Top Sellers",
-                captionStyle: "menu-headers product4TCaption",
-                productStyle: "product4T product4THome",
-                divSelect: "#modTopSellers",
-                divPosition: "replace",
-                imageSize: "&maxx=120&maxy=0",
-                rawJS: {
-                    preInit: function (tout) {
-                        tout.startPos = Math.floor(20 * Math.random(20 - tout.numItems));
-                        $("#Top_Sellers_Block").hide();
-                    }
-                },
-                wrapper: "<div class='PD14T'></div>",
-                inCart: false
-            }
-            ],
-            ProductDetail: [
-                {
+            Home: {
+                tout1: {
                     enable: true,
-                    resultType: 0,
-                    fillMode: "genomic",
+                    resultType: 4,
                     numItems: 4,
-                    caption: "Customers also bought",
+                    caption: "Top Sellers",
                     captionStyle: "menu-headers product4TCaption",
                     productStyle: "product4T product4THome",
-                    divSelect: "#modTopSellers",
+                    divSelect: "#Top_Sellers_Block",
                     divPosition: "replace",
-                    imageSize: "&maxx=120&maxy=0",
-                    rawJS: {
-                        preInit: function (tout) {
-                            _4TellBoost.setCatId(catID);
-                        }
-                    },
-                    wrapper: "<div class='PD14T'></div>",
+                    imageSize: "&maxx=120&maxy=120",
                     inCart: false
-                }, {
-                    enable: false,
+                }
+            },
+            ProductDetail: {
+                tout1: {
+                    enable: true,
                     resultType: 0,
                     numItems: 6,
                     caption: "Customers also bought...",
                     captionStyle: "menu-headers product4TCaption",
                     productStyle: "product4T product4TPD1",
-                    divSelect: ".relatedBlock",
+                    divSelect: "tr:has(td.options-titles):last",
                     divPosition: "below",
                     carousel: {
-                        items: 4,
-                        navigation: true,
-                        navigationText: false,
-                        pagination: false
+                        numVis: 2,
+                        circular: true
                     },
                     showRatings: true,
                     maxImageHeight: 150,
                     imageSize: "&maxx=120&maxy=120",
-                    wrapper: "<div class='PD14T'></div>",
+                    wrapper: "<tr><td colspan='2'></td></tr>",
                     inCart: false
-                }, {
+                },
+                tout2: {
                     enable: true,
                     resultType: 3,
                     numItems: 12,
                     caption: "Customers also viewed...",
                     captionStyle: "titles product4TCaption",
                     productStyle: "product4T product4TPD2",
-                    divSelect: ".relatedBlock",
-                    divPosition: "replace",
+                    divSelect: "#REV4T",
+                    divPosition: "above",
                     carousel: {
-                        items: 4,
-                        navigation: true,
-                        navigationText: false,
-                        pagination: false
+                        numVis: 4,
+                        circular: true
                     },
                     imageSize: "&maxx=120&maxy=120",
                     maxImageHeight: 120,
                     showRatings: true,
-                    wrapper: "<div class='PD14T'></div>",
+                    wrapper: "<tr><td colspan='2'></td></tr>",
                     rawJS: {
-                        preDisplay: function () { }
+                        preDisplay: function () {
+                            $('#REL4T').hide();
+                        }
                     },
                     inCart: false
                 }
-            ],
-            Search: [{
-                enable: false
-            }
-            ],
-            Category: [{
-                enable: true,
-                resultType: 5,
-                fillMode: "genomic",
-                numItems: 4,
-                caption: "Related Top Sellers",
-                captionStyle: "menu-headers product4TCaption",
-                productStyle: "product4T product4THome",
-                divSelect: "#modTopSellers",
-                divPosition: "replace",
-                imageSize: "&maxx=120&maxy=0",
-                showRatings: true,
-                rawJS: {
-                    preInit: function (tout) {
-                        if ("17" == _4TellBoost.Service.catId || "34" == _4TellBoost.Service.catId || "55" == _4TellBoost.Service.catId) {
-                            //TODO Fuck yo encapsulation
-                            _4TellBoost.Service.catId = "";
-                            tout.rotateRecs = true;
-                            tout.startPos = Math.floor(20 * Math.random(20 - tout.numItems));
-                            tout.resultType = 4;
-                        }
+            },
+            Search: {
+                tout1: {
+                    enable: false
+                }
+            },
+            Category: {
+                tout1: {
+                    enable: true,
+                    resultType: 5,
+                    numItems: 4,
+                    caption: "Related Top Sellers",
+                    captionStyle: "menu-headers product4TCaption",
+                    productStyle: "product4T product4THome",
+                    divSelect: "#Top_Sellers_Block",
+                    divPosition: "replace",
+                    imageSize: "&maxx=120&maxy=120",
+                    maxImageHeight: 100,
+                    showRatings: true,
+                    inCart: false
+                }
+            },
+            AddToCart: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 12,
+                    caption: "You may also like...",
+                    captionStyle: "item product4TCaption",
+                    productStyle: "product4T product4TVC",
+                    orientation: "horizontal",
+                    divSelect: "tr:has(#Table5):last",
+                    divPosition: "below",
+                    carousel: {
+                        numVis: 4,
+                        circular: true
                     },
-                    preDisplay: function () {
-                        $("#Top_Sellers_Block").hide();
-                    }
-                },
-                wrapper: "<div class='PD14T'></div>",
-                inCart: false
+                    imageSize: "&maxx=120&maxy=120",
+                    maxImageHeight: 100,
+                    showRatings: true,
+                    wrapper: "<tr><td colspan='6'></td></tr>",
+                    inCart: true
+                }
             }
-            ],
-            AddToCart: [
-            {
-                enable: true,
-                resultType: 0,
-                numItems: 12,
-                caption: "You may also like...",
-                showCaption: false,
-                captionStyle: "item product4TCaption",
-                productStyle: "product4T product4TVC",
-                divSelect: ".productBlockContainer",
-                divPosition: "replace",
-                carousel: {
-                    items: 4,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
-                },
-                imageSize: "&maxx=120&maxy=120",
-                maxImageHeight: 100,
-                showRatings: true,
-                wrapper: "<div class='PD14T'></div>",
-                inCart: true
-            }],
-            Auto: [{
-                enable: true,
-                resultType: 5,
-                fillMode: "genomic",
-                numItems: 4,
-                caption: "Related Top Sellers",
-                captionStyle: "menu-headers product4TCaption",
-                productStyle: "product4T product4THome",
-                divSelect: "#modTopSellers",
-                divPosition: "replace",
-                imageSize: "&maxx=120&maxy=0",
-                maxImageHeight: 100,
-                showRatings: true,
-                rawJS: {
-                    preInit: function (tout) {
-                        if (["55", "92", "1", "34"].indexOf(catID) >= 0) {
-                            tout.rotateRecs = true;
-                            tout.startPos = Math.floor((20 - tout.numItems) * Math.random());
-                            _4TellBoost.setCatId(catID);
-                            tout.resultType = 4;
-                        } else {
-                            tout.enable = false
-                        }
-                    },
-                    preDisplay: function () {
-                        $("#Top_Sellers_Block").hide();
-                    }
-                },
-                wrapper: "<div class='PD14T'></div>",
-                inCart: false
-            }]
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
