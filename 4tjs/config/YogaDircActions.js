c399a1880eb3aae50b927f62c2ad51c2da9d090c
dougadams4 <dougadams4@gmail.com>
2015-09-15 10:40:01 -0700

    live4tjsYogaDircActions.js - initial pull

---
+++ 4tjs/config/YogaDircActions.js
@@ -0,0 +1,6 @@
+﻿(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+
+    window._gaq = window._gaq || [];
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
+//self-invoked namespace that protects $ and undefined internally
