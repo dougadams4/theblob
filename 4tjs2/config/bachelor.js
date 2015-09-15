0587eac7ab02d77dbcac93e064c37bfcf077e0c3
dougadams4 <dougadams4@gmail.com>
2015-09-15 00:57:34 -0700

    snapshot4tjs2bachelor.js - initial pull

--- 4tjs2/config/bachelor.js
+++ 4tjs2/config/bachelor.js
@@ -3,72 +3,53 @@
         SiteInfo: {
             baseURL: "www.bachelorettesuperstore.com",
             alias: "bachelor",
-            GA_UA: "UA-229495-1",
+            GA_UA: "",
             platform: "4TellMv.js",
-            addCartBtnAtts: "form method='post' action='merchant.mvc?Screen=BASK'><input type='hidden' name='Action' value='ADPR'><input type='hidden' name='Quantity' value='1'><input type='image' src='//www.bachelorettesuperstore.com/mm5/css/00000002/buyme.png'></form",
-            pricePrefix: "Price: ",
+            addCartBtnAtts: "input type='button' value='Add to Cart' class='btn' onmouseout=this.className='btn' onmouseover=this.className='btn_over'",
+            addCartImage: "",
+            spacerImage: "/web/assets/images/spacer.gif",
+            emptyImage: "thumbnail.asp?file=/assets/images/default.jpg",
+            pricePrefix: "Our Price: ",
             salePricePrefix: "On Sale: ",
             includeBase: false,
-            siteEnable: true
+            siteEnable: false
         },
         PageSettings: {
             Home: [{
+                enable: false
+            }],
+            ProductDetail: [{
                 enable: true,
-                resultType: 1,
-                numItems: 12,
-                //caption: "<h2><img src='css/00000002/suggestions.gif' width='386' height='48'></h2>",
-                showCaption: false,
-                productStyle: "product4T product4THome",
-                divSelect: ".expanded",
-                divPosition: "replace",
+                resultType: 0,
+                numItems: 2,
+                caption: "Our customers also bought...",
+                productStyle: "product4T product4TPD1",
+                divSelect: "#main4TellContainer",
+                divPosition: "append",
                 carousel: {
                     numVis: 4,
                     circular: true
                 },
                 showRatings: true,
                 maxImageHeight: 150,
-                wrapper: "<div class='HOME4T'></div>",
-                rawJS: {},
-                inCart: false
-            }],
-            ProductDetail: [{
-                enable: true,
-                resultType: 0,
-                numItems: 12,
-                caption: "Customers also bought",
-                captionStyle: "product4TCaption product4TCaptionPD1",
-                productStyle: "product4T product4TPD1",
-                divSelect: ".product-details-3",
-                divPosition: "above",
-                carousel: {
-                    numVis: 2,
-                    circular: true
-                },
-                maxImageHeight: 120,
-                showRatings: true,
                 wrapper: "<div class='PD14T'></div>",
-                rawJS: {
-                    preDisplay: function (tout) {
-                        $(tout.divSelect).prev("div.clear").hide();
-                    }
-                },
+                rawJS: {},
                 inCart: false
             }, {
                 enable: true,
                 resultType: 3,
-                numItems: 15,
-                caption: "<h2><img src='css/00000002/suggestions.gif' width='386' height='48'></h2>",
+                numItems: 4,
+                caption: "Similar Items",
                 productStyle: "product4T product4TPD2",
-                divSelect: "#product-footer",
-                divPosition: "above",
-                carousel: {
-                    numVis: 5,
-                    circular: true
-                },
+                divSelect: "#second4TellContainer",
+                divPosition: "append",
+                setHeight: true,
+                setWidth: false,
+                imageSize: "&maxx=120&maxy=120",
+                maxImageHeight: 120,
                 showRatings: true,
-                maxImageHeight: 150,
-                wrapper: "<div class='PD24T'></div>",
-                rawJS: { preDisplay: function () { $(".related-products").hide(); } },
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
                 inCart: false
             }],
             Search: [{
@@ -77,43 +58,34 @@
             Category: [{
                 enable: true,
                 resultType: 5,
-                //fillMode: "none",
-                numItems: 15,
-                caption: "Related Top Sellers",
-                captionStyle: "product4TCaption product4TCaptionPD1",
+                numItems: 4,
+                caption: "More ideas...",
                 productStyle: "product4T product4TCat",
-                divSelect: "#category-listing",
-                divPosition: "above",
-                carousel: {
-                    numVis: 5,
-                    circular: true
-                },
-                maxImageHeight: 120,
+                divSelect: "#main4TellContainer",
+                divPosition: "append",
+                setHeight: false,
+                setWidth: false,
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
                 showRatings: true,
-                wrapper: "<div class='CAT4T'></div>",
-                rawJS: {
-                    preInit: function (tout) {
-                        tout.enable = tout.enable && $(".product-item").length >= 10;
-                    }
-                },
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
                 inCart: false
             }],
             AddToCart: [{
                 enable: true,
                 resultType: 0,
-                numItems: 15,
+                numItems: 4,
                 caption: "You may also like...",
-                captionStyle: "product4TCaption product4TCaptionPD1",
                 productStyle: "product4T product4TVC",
-                divSelect: "center *:last",
-                divPosition: "above",
-                carousel: {
-                    numVis: 5,
-                    circular: true
-                },
+                divSelect: "#second4TellContainer",
+                divPosition: "append",
+                setHeight: true,
+                setWidth: false,
+                imageSize: "&maxx=100&maxy=100",
                 maxImageHeight: 100,
                 showRatings: true,
-                wrapper: "<div class='VC4T'></div>",
+                wrapper: "<div class='PD14T'></div>",
                 rawJS: {},
                 inCart: true
             }]
