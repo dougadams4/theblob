648c61a85cf66837b9e90808770006a516b416c8
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:24:51 -0700

    snapshot4tjsLydiasUn.js - initial pull

--- 4tjs/config/LydiasUn.js
+++ 4tjs/config/LydiasUn.js
@@ -14,120 +14,126 @@
             siteEnable: true
         },
         PageSettings: {
-            Home: [{
-                enable: false
-            }],
-            ProductDetail: [{
-                enable: true,
-                resultType: 0,
-                numItems: 3,
-                caption: "More Ideas:",
-                productStyle: "product4T product4TPD1",
-                divSelect: "#tout1_pdp_4Tell",
-                divPosition: "replace",
-                showRatings: true,
-                maxImageHeight: 150,
-                wrapper: "<div class='PD14T'></div>",
-                rawJS: {
-                    preInit: function (tout) {
-                        tout.columns.custom = { topSale: "tsp", topPrice: "tpr" };
-                        _4TellBoost.UserData.data.Viewed.push(_4TellBoost.Service.productList[0]);
-                        _4TellBoost.UserData.save();
-                    },
-                    perProduct: function (itemdata) {
-                        itemdata.rating = itemdata.rating > -1 ? itemdata.rating : "-1";
-                        if (itemdata.topPrice && itemdata.price) itemdata.price = itemdata.price + " - " + itemdata.topPrice;
-                        if (itemdata.topSale && itemdata.salePrice) itemdata.salePrice = itemdata.salePrice + " - " + itemdata.topSale;
-                    }
-                },
-                inCart: false
-            }, {
-                enable: false
-            }],
-            Search: [{
-                enable: true,
-                resultType: 5,
-                numItems: 6,
-                fillMode: "genomic",
-                caption: "Recommendations for You",
-                captionStyle: "product4TCaption product4TCaptionSearch",
-                productStyle: "product4T product4TSearch",
-                divSelect: "#tout1_srch_4Tell",
-                divPosition: "replace",
-                showRatings: true,
-                maxImageHeight: 150,
-                wrapper: "<div class='CAT4T'></div>",
-                rawJS: {
-                    preInit: function (tout) {
-                        if ("undefined" !== typeof (Array.prototype.map)) {
-                            _4TellBoost.Service.productList = _4TellBoost.Service.productList.map(function (element) {
-                                return element.match(/\w+/)[0];
-                            });
+            Home: {
+                tout1: {
+                    enable: false
+                }
+            },
+            ProductDetail: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 3,
+                    caption: "More Ideas:",
+                    productStyle: "product4T product4TPD1",
+                    divSelect: "#tout1_pdp_4Tell",
+                    divPosition: "replace",
+                    showRatings: true,
+                    maxImageHeight: 150,
+                    wrapper: "<div class='PD14T'></div>",
+                    rawJS: {
+                        preInit: function (tout) {
+                            tout.columns.custom = { topSale: "tsp", topPrice: "tpr" };
+                        },
+                        perProduct: function (itemdata) {
+                            itemdata.rating = itemdata.rating > -1 ? itemdata.rating : "-1";
+                            if (itemdata.topPrice && itemdata.price) itemdata.price = itemdata.price + " - " + itemdata.topPrice;
+                            if (itemdata.topSale && itemdata.salePrice) itemdata.salePrice = itemdata.salePrice + " - " + itemdata.topSale;
                         }
-                        tout.enable = tout.enable && (_4TellBoost.Service.productCount >= 20);
-                        _4TellBoost.Service.productList = [];
-                        _4TellBoost.Service.productCount = 0;
-                        tout.columns.custom = { topSale: "tsp", topPrice: "tpr" };
                     },
-                    perProduct: function (itemdata) {
-                        itemdata.rating = itemdata.rating > -1 ? itemdata.rating : "-1";
-                        if (itemdata.topPrice && itemdata.price) itemdata.price = itemdata.price + " - " + itemdata.topPrice;
-                        if (itemdata.topSale && itemdata.salePrice) itemdata.salePrice = itemdata.salePrice + " - " + itemdata.topSale;
-                    }
+                    inCart: false
                 },
-                inCart: false
-            }],
-            Category: [{
-                enable: false
-            }],
-            AddToCart: [{
-                enable: true,
-                resultType: 0,
-                numItems: 4,
-                caption: "Others Also Bought",
-                captionStyle: "product4TCaption product4TCaptionVC",
-                productStyle: "product4T product4TVC",
-                divSelect: "#tout1_cart_4Tell",
-                divPosition: "replace",
-                maxImageHeight: 100,
-                showRatings: true,
-                wrapper: "<div class='VC4T'></div>",
-                rawJS: {
-                    preInit: function (tout) {
+                tout2: {
+                    enable: false
+                }
+            },
+            Search: {
+                tout1: {
+                    enable: true,
+                    resultType: 5,
+                    numItems: 6,
+                    caption: "Recommendations for You",
+                    captionStyle: "product4TCaption product4TCaptionSearch",
+                    productStyle: "product4T product4TSearch",
+                    divSelect: "#tout1_srch_4Tell",
+                    divPosition: "replace",
+                    showRatings: true,
+                    maxImageHeight: 150,
+                    wrapper: "<div class='CAT4T'></div>",
+                    rawJS: {
+                        preInit: function (tout) {
+                            if ("undefined" != typeof (Array.prototype.map)) {
+                                _4TellBoost.Service.productList = _4TellBoost.Service.productList.map(function (element) {
+                                    return element.match(/\w+/)[0];
+                                });
+                            }
+                            tout.columns.custom = { topSale: "tsp", topPrice: "tpr" };
+                        },
+                        perProduct: function (itemdata) {
+                            itemdata.rating = itemdata.rating > -1 ? itemdata.rating : "-1";
+                            if (itemdata.topPrice && itemdata.price) itemdata.price = itemdata.price + " - " + itemdata.topPrice;
+                            if (itemdata.topSale && itemdata.salePrice) itemdata.salePrice = itemdata.salePrice + " - " + itemdata.topSale;
+                        }
+                    },
+                    inCart: false
+                },
+            },
+            Category: {
+                tout1: {
+                    enable: false
+                }
+            },
+            AddToCart: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 4,
+                    caption: "Others Also Bought",
+                    captionStyle: "product4TCaption product4TCaptionVC",
+                    productStyle: "product4T product4TVC",
+                    divSelect: "#tout1_cart_4Tell",
+                    divPosition: "replace",
+                    maxImageHeight: 100,
+                    showRatings: true,
+                    wrapper: "<div class='VC4T'></div>",
+                    rawJS: {
+                        preInit: function (tout) {
 
-                        tout.columns.custom = { topSale: "tsp", topPrice: "tpr" };
+                            tout.columns.custom = { topSale: "tsp", topPrice: "tpr" };
+                        },
+                        perProduct: function (itemdata) {
+                            itemdata.rating = itemdata.rating > -1 ? itemdata.rating : "-1";
+                            if (itemdata.topPrice && itemdata.price) itemdata.price = itemdata.price + " - " + itemdata.topPrice;
+                            if (itemdata.topSale && itemdata.salePrice) itemdata.salePrice = itemdata.salePrice + " - " + itemdata.topSale;
+                        }
                     },
-                    perProduct: function (itemdata) {
-                        itemdata.rating = itemdata.rating > -1 ? itemdata.rating : "-1";
-                        if (itemdata.topPrice && itemdata.price) itemdata.price = itemdata.price + " - " + itemdata.topPrice;
-                        if (itemdata.topSale && itemdata.salePrice) itemdata.salePrice = itemdata.salePrice + " - " + itemdata.topSale;
-                    }
+                    inCart: true
                 },
-                inCart: true
-            }, {
-                enable: true,
-                resultType: 4,
-                numItems: 4,
-                caption: "Featured Items",
-                captionStyle: "product4TCaption product4TCaptionVC",
-                productStyle: "product4T product4TVC",
-                divSelect: "#tout1_cart_4Tell, .VC4T",
-                divPosition: "below",
-                maxImageHeight: 100,
-                showRatings: true,
-                wrapper: "<div class='VC4T'></div>",
-                rawJS: {
-                    preInit: function (tout) {
-                        tout.columns.custom = { topSale: "tsp", topPrice: "tpr" };
+                tout2: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 4,
+                    caption: "Featured Items",
+                    captionStyle: "product4TCaption product4TCaptionVC",
+                    productStyle: "product4T product4TVC",
+                    divSelect: "#tout1_cart_4Tell, .VC4T",
+                    divPosition: "below",
+                    maxImageHeight: 100,
+                    showRatings: true,
+                    wrapper: "<div class='VC4T'></div>",
+                    rawJS: {
+                        preInit: function (tout) {
+                            tout.columns.custom = { topSale: "tsp", topPrice: "tpr" };
+                        },
+                        perProduct: function (itemdata) {
+                            itemdata.rating = itemdata.rating > -1 ? itemdata.rating : "-1";
+                            if (itemdata.topPrice && itemdata.price) itemdata.price = itemdata.price + " - " + itemdata.topPrice;
+                            if (itemdata.topSale && itemdata.salePrice) itemdata.salePrice = itemdata.salePrice + " - " + itemdata.topSale;
+                        }
                     },
-                    perProduct: function (itemdata) {
-                        itemdata.rating = itemdata.rating > -1 ? itemdata.rating : "-1";
-                        if (itemdata.topPrice && itemdata.price) itemdata.price = itemdata.price + " - " + itemdata.topPrice;
-                        if (itemdata.topSale && itemdata.salePrice) itemdata.salePrice = itemdata.salePrice + " - " + itemdata.topSale;
-                    }
-                },
-                inCart: true
-            }]
+                    inCart: true
+                }
+            }
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
