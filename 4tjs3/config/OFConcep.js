a7132d90c2d05cc5ab8875f1bc5391a31c4ab1a9
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:41:36 -0700

    snapshot4tjs3OFConcep.js - initial pull

--- 4tjs3/config/OFConcep.js
+++ 4tjs3/config/OFConcep.js
@@ -5,97 +5,80 @@
             alias: "OFConcep",
             GA_UA: "UA-5985949-1",
             platform: "4TellVs.js",
-            addCartBtnAtts: "a value='Add To Cart'>Add to Cart</a",
-            //addCartImage: "/v/vspfiles/templates/ofconcepts/images/buttons/btn_addtocart_small.gif",
-            spacerImage: "/v/vspfiles/templates/ofconcepts/images/clear1x1.gif",
-            emptyImage: "/v/vspfiles/templates/ofconcepts/images/nophoto.gif",
+            addCartBtnAtts: "img class='vCSS_input_addtocart' alt='Add to Cart'",
+            addCartImage: "/v/vspfiles/templates/OFConcepts/images/buttons/btn_addtocart_small.gif",
+            spacerImage: "/v/vspfiles/templates/OFConcepts/images/clear1x1.gif",
+            emptyImage: "/v/vspfiles/templates/OFConcepts/images/nophoto.gif",
             pricePrefix: "Our Price: ",
             salePricePrefix: "On Sale: ",
             includeBase: false,
             siteEnable: true
         },
         PageSettings: {
-            Home: [{
-                enable: false
-            }],
-            ProductDetail: [{
-                enable: false,
-                resultType: 0,
-                numItems: 6,
-                caption: "Our customers also bought",
-                captionStyle: "titles product4TCaption",
-                productStyle: "product4T product4TPD1",
-                divSelect: "table#v65-product-parent",
-                divPosition: "below",
-                carousel: {
-                    items: 3,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
-                },
-                showRatings: true,
-                maxImageHeight: 150,
-                wrapper: "<div class='PD14T'></div>",
-                inCart: false
+            Home: {
+                tout1: {
+                    enable: false
+                }
             },
-                {
+            ProductDetail: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 6,
+                    caption: "Our customers also bought...",
+                    captionStyle: "titles product4TCaption",
+                    productStyle: "product4T product4TPD1",
+                    divSelect: "table.colors_pricebox",
+                    divPosition: "below",
+                    carousel: {
+                        circular: true,
+                        numVis: 3
+                    },
+                    showRatings: true,
+                    maxImageHeight: 150,
+                    wrapper: "<table width='450px' align='right' style='border-spacing: 5px'><tr><td style='border: 1px solid #DFE0E0;'></td></tr></table>",
+                    inCart: false
+                },
+                tout2: {
                     enable: true,
                     resultType: 3,
-                    numItems: 12,
-                    caption: "Our customers also viewed",
+                    numItems: 4,
+                    caption: "Our customers also viewed...",
                     productStyle: "product4T product4TPD2",
                     divSelect: "table#v65-product-related",
                     divPosition: "replace",
-                    carousel: {
-                        items: 4,
-                        scrollPerPage: true,
-                        navigation: true,
-                        navigationText: false,
-                        pagination: false
-                    },
                     maxImageHeight: 120,
                     showRatings: true,
-                    wrapper: "<div class='PD14T'></div>",
-                    rawJS: {
-                        preDisplay: function (tout) {
-                            jQuery(tout.divSelect).parents("table").css("table-layout", "fixed");
-                        }
-                    },
+                    wrapper: "<tr><td style='border: 1px solid #DFE0E0;'></td></tr>",
                     inCart: false
-                }],
-            Category: [{
-                enable: false
-            }],
-            Search: [{
-                enable: false
-            }],
-            AddToCart: [{
-                enable: true,
-                resultType: 0,
-                numItems: 12,
-                caption: "You may also like",
-                //captionStyle: "checkout-headers product4TCaption product4TCaptionVC",
-                productStyle: "product4T product4TVC",
-                divSelect: "#v65-cart-checkout-parent ",
-                divPosition: "below",
-                carousel: {
-                    items: 3,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
-                },
-                maxImageHeight: 100,
-                showRatings: true,
-                wrapper: "<div class='PD14T'></div>",
-                rawJS: {
-                    preDisplay: function (tout) {
-                        jQuery(tout.divSelect).parents("table").css("table-layout", "fixed");
-                    }
-                },
-                inCart: true
-            }]
+                }
+            },
+            Category: {
+                tout1: {
+                    enable: false
+                }
+            },
+            Search: {
+                tout1: {
+                    enable: false
+                }
+            },
+            AddToCart: {
+                tout1: {
+                    enable: false,
+                    resultType: 0,
+                    numItems: 3,
+                    caption: "You may also like...",
+                    captionStyle: "checkout-headers product4TCaption product4TCaptionVC",
+                    productStyle: "product4T product4TVC",
+                    divSelect: "#v65-cart-checkout-parent td:first",
+                    divPosition: "replace",
+                    maxImageHeight: 100,
+                    showRatings: true,
+                    wrapper: "<td colspan='3' style='border: 1px solid #DFE0E0;'></td>",
+                    inCart: true
+                }
+            }
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
