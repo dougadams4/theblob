b78127b57137147bd523d5946ff11af5aa3b2de8
dougadams4 <dougadams4@gmail.com>
2015-09-15 07:42:13 -0700

    live4tjskaratesu.js - initial pull

---
+++ 4tjs/config/karatesu.js
@@ -0,0 +1,129 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.CONFIG = {
+        SiteInfo: {
+            baseURL: "www.karatesupply.com",
+            alias: "karatesu",
+            GA_UA: "UA-227793-1",
+            platform: "4Tell3d.js",
+            addCartBtnAtts: "input type='button' value='Add to Cart' class='btn' onmouseout=this.className='btn' onmouseover=this.className='btn_over'",
+            addCartImage: "",
+            spacerImage: "/web/assets/images/spacer.gif",
+            emptyImage: "thumbnail.asp?file=/assets/images/default.jpg",
+            pricePrefix: "Our Price: ",
+            salePricePrefix: "On Sale: ",
+            includeBase: false,
+            siteEnable: true
+        },
+        PageSettings: {
+            Home: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "Personal Recommendations",
+                productStyle: "product4T product4THome",
+                divSelect: ".productBlockContainer ",
+                divPosition: "replace",
+                carousel: {
+                    items: 4,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                showRatings: true,
+                imageSize: "&maxx=180&maxy=180",
+                maxImageHeight: 150,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {
+                    preDisplay: function (tout) {
+                        jQuery(tout.divSelect).hide();
+                    }
+                },
+                inCart: false
+            }],
+            ProductDetail: [{
+                enable: true,
+                resultType: 0,
+                numItems: 4,
+                caption: "Customers also bought",
+                productStyle: "product4T product4TPD1",
+                divSelect: "#modMailist",
+                divPosition: "below",
+                showRatings: true,
+                imageSize: "&maxx=180&maxy=0",
+                maxImageHeight: 150,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
+                inCart: false
+            }, {
+                enable: true,
+                resultType: 3,
+                numItems: 12,
+                caption: "Customers also bought",
+                productStyle: "product4T product4TPD2",
+                divSelect: ".relatedBlock",
+                divPosition: "replace",
+                carousel: {
+                    items: 4,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                imageSize: "&maxx=120&maxy=120",
+                maxImageHeight: 120,
+                showRatings: true,
+                wrapper: "<div class='PD24T'></div>",
+                rawJS: {},
+                inCart: false
+            }],
+            Search: [{
+                enable: false
+            }],
+            Category: [{
+                enable: false,
+                resultType: 5,
+                numItems: 4,
+                caption: "More ideas...",
+                productStyle: "product4T product4TCat",
+                divSelect: "#main4TellContainer",
+                divPosition: "append",
+                setHeight: false,
+                setWidth: false,
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                wrapper: "<div class='CAT4T'></div>",
+                rawJS: {},
+                inCart: false
+            }],
+            AddToCart: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "You may also like",
+                productStyle: "product4T product4TVC",
+                divSelect: ".displayPromotions ",
+                divPosition: "above",
+                carousel: {
+                    items: 4,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                wrapper: "<div class='VC4T'></div>",
+                rawJS: {
+                    preDisplay: function () {
+                        jQuery(".productBlockContainer").hide();
+                        jQuery(".displayPromotions ~ .header").hide();
+                    }
+                },
+                inCart: true
+            }]
+        }
+    }
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
