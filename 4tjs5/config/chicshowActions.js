51fadc2ac2f7fd84d7dfd4401e82ab462f1f6bdd
dougadams4 <dougadams4@gmail.com>
2015-09-15 01:36:50 -0700

    live4tjs5chicshowActions.js - initial pull

---
+++ 4tjs5/config/chicshowActions.js
@@ -0,0 +1,19 @@
+﻿(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+
+    _4TellBoost.getBuyBtn = function (tout, itemdata) {
+        var buyWrapper = $("<div class='productBuy' />");
+        var buyBtn = $(_4TellBoost.SiteInfo.addCartBtnAtts);
+        if (buyBtn)
+            buyBtn.appendTo(buyWrapper);
+        var newAddress = itemdata.pageLink;
+
+        //can't wrap with a-href in IE so add new address to the onclick handler
+        buyBtn.click(function () {
+            _4TellBoost.TrackGA(0, _4TellBoost.getPageType(tout), itemdata.resultType, itemdata.title, null, newAddress);
+        });
+        return buyWrapper;
+    };
+
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
+//self-invoked namespace that protects $ and undefined internally
