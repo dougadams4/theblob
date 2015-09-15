1f2c16d4935314d3c9108e08b8a0bbb2bc086f8e
dougadams4 <dougadams4@gmail.com>
2015-09-15 03:15:10 -0700

    live4tjs4GentlemaActions.js - initial pull

---
+++ 4tjs4/config/GentlemaActions.js
@@ -0,0 +1,27 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+    //_4TellBoost.Service.address = "stage.4-Tell.net/Boost2.0/";
+    _4TellBoost.getBuyBtn = function (tout, itemdata) {
+        var buyWrapper = $("<div class='productBuy' />");
+        var buyBtn = $(_4TellBoost.SiteInfo.addCartBtnAtts);
+        if (_4TellBoost.SiteInfo.addCartImage) {
+            var buyBtnImage = "";
+            if (_4TellBoost.SiteInfo.includeBase) {
+                buyBtnImage = "//" + _4TellBoost.SiteInfo.baseURL + "/"
+            }
+            buyBtnImage += _4TellBoost.SiteInfo.addCartImage;
+            buyBtn.attr("src", buyBtnImage)
+        }
+        if (buyBtn) {
+            buyBtn.appendTo(buyWrapper)
+        }
+        var newAddress = itemdata.pageLink;
+
+        buyBtn.click(function () {
+            _4TellBoost.addCartItem(itemdata.productID);
+            _4TellBoost.TrackClick(tout.toutType, itemdata.productID, newAddress);
+        });
+        return buyWrapper
+    };
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
+
