96480a79c5eec8e2b9401e54ee12790199bb3f18
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:50:34 -0700

    live4tjs4pkyspkys.js - initial pull

---
+++ 4tjs4/config/pkyspkys.js
@@ -0,0 +1,107 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.CONFIG = {
+        SiteInfo: {
+            baseURL: "www.CLIENTURL.com",
+            alias: "pkyspkys",
+            GA_UA: "",
+            platform: "4Tell3d.js",
+            addCartBtnAtts: "input type='button' value='Add to Cart' class='btn' onmouseout=this.className='btn' onmouseover=this.className='btn_over'",
+            addCartImage: "",
+            spacerImage: "/web/assets/images/spacer.gif",
+            emptyImage: "thumbnail.asp?file=/assets/images/default.jpg",
+            pricePrefix: "Our Price: ",
+            salePricePrefix: "On Sale: ",
+            includeBase: false,
+            siteEnable: false
+        },
+        PageSettings: {
+            Home: [{
+                enable: false
+            }],
+            ProductDetail: [{
+                enable: true,
+                resultType: 0,
+                numItems: 4,
+                caption: "Customers also bought...",
+                productStyle: "product4T product4TPD1",
+                divSelect: "#TOP_SELLERS_BLOCK, #column2 > table:first",
+                divPosition: "replace",
+                showRatings: true,
+                imageSize: "&maxx=120&maxy=120",
+                maxImageHeight: 150,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
+                inCart: false
+            }, {
+                enable: true,
+                resultType: 3,
+                numItems: 12,
+                caption: "Similar Items",
+                productStyle: "product4T product4TPD2",
+                divSelect: "#REL4T",
+                divPosition: "replace",
+                carousel: {
+                    items: 4,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                imageSize: "&maxx=120&maxy=120",
+                maxImageHeight: 120,
+                showRatings: true,
+                wrapper: "<tr><td colspan='2'><div class='PD24T'></div></td></tr>",
+                rawJS: {
+                    preDisplay: function (tout) {
+                        $(tout.divSelect).parents("table").css("table-layout", "fixed");
+                    }
+                },
+                inCart: false
+            }],
+            Search: [{
+                enable: false
+            }],
+            Category: [{
+                enable: false,
+                resultType: 5,
+                numItems: 4,
+                caption: "More ideas...",
+                productStyle: "product4T product4TCat",
+                divSelect: "#main4TellContainer",
+                divPosition: "append",
+                setHeight: false,
+                setWidth: false,
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                wrapper: "<div class='CAT4T'></div>",
+                rawJS: {},
+                inCart: false
+            }],
+            AddToCart: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "You may also like...",
+                productStyle: "product4T product4TVC",
+                divSelect: "form[name=recalculate] ~ script",
+                divPosition: "above",
+                carousel: {
+                    items: 4,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                wrapper: "<tr><td colspan='6'><div class='VC4T'></div></td></tr>",
+                rawJS: {
+                    preDisplay: function (tout) {
+                        $(tout.divSelect).parents("table").css("table-layout", "fixed");
+                    }
+                },
+                inCart: true
+            }]
+        }
+    }
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
