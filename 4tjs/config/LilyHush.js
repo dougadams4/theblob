e7a75338d9dfa66646d62de7ea9c37e3fc001df0
dougadams4 <dougadams4@gmail.com>
2015-09-15 07:56:19 -0700

    snapshot4tjsLilyHush.js - initial pull

--- 4tjs/config/LilyHush.js
+++ 4tjs/config/LilyHush.js
@@ -22,47 +22,50 @@
             siteEnable: true
         },
         PageSettings: {
-            Home: [{
-                enable: true,
-                resultType: 1,
-                numItems: 15,
-                caption: "You May Also Like...",
-                captionStyle: "titles product4TCaption",
-                productStyle: "product4T product4THome",
-                divSelect: "#HOME4T",
-                divPosition: "replace",
-                carousel: {
-                    numVis: 5,
-                    circular: true
-                },
-                maxImageHeight: 170,
-                showRatings: true,
-                showBuyButton: false,
-                showPrice: false,
-                wrapper: "<div id='HOME4T'></div>",
-                inCart: false
-            }],
-            ProductDetail: [{
-                enable: true,
-                resultType: 0,
-                numItems: 12,
-                caption: "Our Customers Also Bought...",
-                captionStyle: "titles product4TCaption",
-                productStyle: "product4T product4TPD1",
-                divSelect: "div.ProductMain",
-                divPosition: "below",
-                carousel: {
-                    numVis: 4,
-                    circular: true
-                },
-                showRatings: true,
-                showBuyButton: false,
-                showPrice: false,
-                maxImageHeight: 150,
-                wrapper: "<div class='PD4T'></div>",
-                inCart: false
+            Home: {
+                tout1: {
+                    enable: true,
+                    resultType: 1,
+                    numItems: 15,
+                    caption: "You May Also Like...",
+                    captionStyle: "titles product4TCaption",
+                    productStyle: "product4T product4THome",
+                    divSelect: "#HOME4T",
+                    divPosition: "replace",
+                    carousel: {
+                        numVis: 5,
+                        circular: true
+                    },
+                    maxImageHeight: 170,
+                    showRatings: true,
+                    showBuyButton: false,
+                    showPrice: false,
+                    wrapper: "<div id='HOME4T'></div>",
+                    inCart: false
+                }
             },
-                {
+            ProductDetail: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 12,
+                    caption: "Our Customers Also Bought...",
+                    captionStyle: "titles product4TCaption",
+                    productStyle: "product4T product4TPD1",
+                    divSelect: "div.ProductMain",
+                    divPosition: "below",
+                    carousel: {
+                        numVis: 4,
+                        circular: true
+                    },
+                    showRatings: true,
+                    showBuyButton: false,
+                    showPrice: false,
+                    maxImageHeight: 150,
+                    wrapper: "<div class='PD4T'></div>",
+                    inCart: false
+                },
+                tout2: {
                     enable: true,
                     resultType: 3,
                     numItems: 10,
@@ -87,67 +90,75 @@
                         }
                     },
                     inCart: false
-                }],
-            Category: [{
-                enable: true,
-                resultType: 5,
-                numItems: 15,
-                caption: "Our Customers Also Bought...",
-                captionStyle: "titles product4TCaption",
-                productStyle: "product4T product4TCat",
-                divSelect: "div.CAT4T",
-                divPosition: "replace",
-                carousel: {
-                    numVis: 5,
-                    circular: true
-                },
-                showRatings: true,
-                showBuyButton: false,
-                showPrice: false,
-                maxImageHeight: 150,
-                wrapper: "<div class='CAT4T'></div>",
-                inCart: false
-            }],
-            Search: [{
-                enable: true,
-                resultType: 5,
-                numItems: 15,
-                caption: "Our Customers Also Bought...",
-                captionStyle: "titles product4TCaption",
-                productStyle: "product4T product4TSearch",
-                divSelect: "div.SRCH4T",
-                divPosition: "replace",
-                carousel: {
-                    numVis: 5,
-                    circular: true
-                },
-                showRatings: true,
-                showBuyButton: false,
-                showPrice: false,
-                maxImageHeight: 150,
-                wrapper: "<div class='SRCH4T'></div>",
-                inCart: false
-            }],
-            AddToCart: [{
-                enable: true,
-                resultType: 0,
-                numItems: 15,
-                caption: "You May Also Like...",
-                captionStyle: "titles product4TCaption",
-                productStyle: "product4T product4TVC",
-                divSelect: "#SuggestiveCartContent",
-                divPosition: "replace",
-                carousel: {
-                    numVis: 5,
-                    circular: true
-                },
-                wrapper: "<div style='float: left; width: 100%;'></div>",
-                maxImageHeight: 100,
-                showRatings: true,
-                showBuyButton: false,
-                showPrice: false,
-                inCart: false
-            }]
+                }
+            },
+            Category: {
+                tout1: {
+                    enable: true,
+                    resultType: 5,
+                    numItems: 15,
+                    caption: "Our Customers Also Bought...",
+                    captionStyle: "titles product4TCaption",
+                    productStyle: "product4T product4TCat",
+                    divSelect: "div.CAT4T",
+                    divPosition: "replace",
+                    carousel: {
+                        numVis: 5,
+                        circular: true
+                    },
+                    showRatings: true,
+                    showBuyButton: false,
+                    showPrice: false,
+                    maxImageHeight: 150,
+                    wrapper: "<div class='CAT4T'></div>",
+                    inCart: false
+                }
+            },
+            Search: {
+                tout1: {
+                    enable: true,
+                    resultType: 5,
+                    numItems: 15,
+                    caption: "Our Customers Also Bought...",
+                    captionStyle: "titles product4TCaption",
+                    productStyle: "product4T product4TSearch",
+                    divSelect: "div.SRCH4T",
+                    divPosition: "replace",
+                    carousel: {
+                        numVis: 5,
+                        circular: true
+                    },
+                    showRatings: true,
+                    showBuyButton: false,
+                    showPrice: false,
+                    maxImageHeight: 150,
+                    wrapper: "<div class='SRCH4T'></div>",
+                    inCart: false
+                }
+
+            },
+            AddToCart: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 15,
+                    caption: "You May Also Like...",
+                    captionStyle: "titles product4TCaption",
+                    productStyle: "product4T product4TVC",
+                    divSelect: "#SuggestiveCartContent",
+                    divPosition: "replace",
+                    carousel: {
+                        numVis: 5,
+                        circular: true
+                    },
+                    wrapper: "<div style='float: left'></div>",
+                    maxImageHeight: 100,
+                    showRatings: true,
+                    showBuyButton: false,
+                    showPrice: false,
+                    inCart: false
+                }
+            }
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
