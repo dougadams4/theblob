9f112acea9dc74dd6beec639b8ad185c88a30ea9
dougadams4 <dougadams4@gmail.com>
2015-09-15 01:03:09 -0700

    live4tjsBestinsoActions.js - initial pull

---
+++ 4tjs/config/BestinsoActions.js
@@ -0,0 +1,34 @@
+﻿(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+
+    $(document).ready(function () {
+        if ($("#info-pain-div").length) {
+            _4TellBoost.setPageType("Category");
+            _4TellBoost.setCatId("100");
+            $("form[name=frmsortby]").attr("action", "view_category.asp?cat=100#prod");
+        }
+    });
+    _4TellBoost.getBuyBtn = function (tout, itemdata) {
+        var buyWrapper = $("<div class='productBuy' />");
+        var buyBtn = $("<" + _4TellBoost.SiteInfo.addCartBtnAtts + " />");
+        if (_4TellBoost.SiteInfo.addCartImage) {
+            var buyBtnImage = '';
+            if (_4TellBoost.SiteInfo.includeBase)
+                buyBtnImage = '//' + _4TellBoost.SiteInfo.baseURL + '/';
+            buyBtnImage += _4TellBoost.SiteInfo.addCartImage;
+            buyBtn.attr("src", buyBtnImage);
+        }
+        if (buyBtn)
+            buyBtn.appendTo(buyWrapper);
+        var newAddress = itemdata.pageLink;
+
+
+        //can't wrap with a-href in IE so add new address to the onclick handler
+        buyBtn.click(function () {
+            //alert(tout.toutType, itemdata.productID, newAddress);
+            _4TellBoost.TrackClick(tout.toutType, itemdata.productID, newAddress);
+        });
+        return buyWrapper;
+    };
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
+//self-invoked namespace that protects $ and undefined internally
