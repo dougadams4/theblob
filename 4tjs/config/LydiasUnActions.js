b77a237cc3e1732d2234cb9d3656c3864e014542
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:23:54 -0700

    live4tjsLydiasUnActions.js - initial pull

---
+++ 4tjs/config/LydiasUnActions.js
@@ -0,0 +1,50 @@
+﻿(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+
+    _4TellBoost.getRatingImage = function (rating) {
+        var rateVal = parseFloat(rating);
+        if (isNaN(rateVal) || rateVal < 0) return $("<div class='ratingImage'/>");
+        //ignore negative and NaN ratings.
+        rateVal = Math.min(rateVal, 5);
+
+        var imagePath = "";
+        var star = String(Math.floor(rateVal));
+        var ratingDiv = $("<div class='ratingImage rate'/>");
+        ratingDiv.addClass("rate" + star);
+        return ratingDiv;
+    };
+
+    _4TellBoost.addCartItem = function (id) {
+        if (!id || (id.substring(0, 1) === "[")) return;
+        id = id.match(/\w+/)[0];
+        _4TellBoost.Service.cartList[_4TellBoost.Service.cartCount] = id;
+        _4TellBoost.Service.cartCount++;
+        if (jQuery.inArray(id, _4TellBoost.UserData.cart) === -1) {
+            _4TellBoost.UserData.cart.unshift(id);
+            _4TellBoost.UserData.save();
+        }
+    };
+    _4TellBoost.getBuyBtn = function (tout, itemdata) {
+        var buyWrapper = $("<div class='productBuy' />");
+        var buyBtn = $("<" + _4TellBoost.SiteInfo.addCartBtnAtts + " />");
+        if (_4TellBoost.SiteInfo.addCartImage) {
+            var buyBtnImage = '';
+            if (_4TellBoost.SiteInfo.includeBase)
+                buyBtnImage = '//' + _4TellBoost.SiteInfo.baseURL + '/';
+            buyBtnImage += _4TellBoost.SiteInfo.addCartImage;
+            buyBtn.attr("src", buyBtnImage);
+        }
+        if (buyBtn)
+            buyBtn.appendTo(buyWrapper);
+        var newAddress = itemdata.pageLink;
+
+
+        //can't wrap with a-href in IE so add new address to the onclick handler
+        buyBtn.click(function () {
+            //alert(tout.toutType, itemdata.productID, newAddress);
+            _4TellBoost.TrackClick(tout.toutType, itemdata.productID, newAddress);
+        });
+        return buyWrapper;
+    };
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
+//self-invoked namespace that protects $ and undefined internally
