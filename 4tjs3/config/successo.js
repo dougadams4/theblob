6f8c923332756eb9e733c4952d7a82cdb9747c9e
dougadams4 <dougadams4@gmail.com>
2015-09-15 09:28:10 -0700

    live4tjs3successo.js - initial pull

---
+++ 4tjs3/config/successo.js
@@ -0,0 +1,109 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.CONFIG = {
+        SiteInfo: {
+            baseURL: "www.CLIENTURL.com",
+            alias: "successo",
+            GA_UA: "UA-1071924-1",
+            platform: "4TellDIY.js",
+            addCartBtnAtts: "input type='button' value='See Details'",
+            addCartImage: "",
+            spacerImage: "/web/assets/images/spacer.gif",
+            emptyImage: "thumbnail.asp?file=/assets/images/default.jpg",
+            pricePrefix: "",
+            salePricePrefix: "",
+            includeBase: false,
+            siteEnable: true
+        },
+        PageSettings: {
+            Home: [{
+                enable: false
+            }],
+            ProductDetail: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "Customers also bought:",
+                productStyle: "product4T product4TPD1",
+                divSelect: "#tout1_pdp_4Tell",
+                divPosition: "replace",
+                carousel: {
+                    items: 3,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                showPrice: false,
+                showRatings: true,
+                maxImageHeight: 150,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
+                inCart: false
+            }, {
+                enable: true,
+                resultType: 3,
+                numItems: 9,
+                caption: "More ideas:",
+                productStyle: "product4T product4TPD2",
+                divSelect: ".product_pg_upsell:has(.upsell_pics)",
+                divPosition: "above",
+                carousel: {
+                    items: 3,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                maxImageHeight: 120,
+                showRatings: true,
+                showPrice: false,
+                wrapper: "<div class='PD24T'></div>",
+                rawJS: {
+                    preDisplay: function (tout) {
+                        //jQuery(tout.divSelect).hide();
+                    }
+                },
+                inCart: false
+            }],
+            Search: [{
+                enable: false
+            }],
+            Category: [{
+                enable: true,
+                resultType: 5,
+                numItems: 4,
+                caption: "More ideas...",
+                productStyle: "product4T product4TCat",
+                divSelect: "#main4TellContainer",
+                divPosition: "append",
+                setHeight: false,
+                setWidth: false,
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                showPrice: false,
+                wrapper: "<div class='CAT4T'></div>",
+                rawJS: {},
+                inCart: false
+            }],
+            AddToCart: [{
+                enable: true,
+                resultType: 0,
+                numItems: 4,
+                caption: "You may also like...",
+                productStyle: "product4T product4TVC",
+                divSelect: "#second4TellContainer",
+                divPosition: "append",
+                setHeight: true,
+                setWidth: false,
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                showPrice: false,
+                wrapper: "<div class='VC4T'></div>",
+                rawJS: {},
+                inCart: true
+            }]
+        }
+    }
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
