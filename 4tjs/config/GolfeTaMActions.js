daaf69922c12da58f7871d61d90a0ed6b03e7a85
dougadams4 <dougadams4@gmail.com>
2015-09-15 03:26:23 -0700

    snapshot4tjsGolfeTaMActions.js - initial pull

---
+++ 4tjs/config/GolfeTaMActions.js
@@ -0,0 +1,18 @@
+﻿(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+    $(document).ready(function () {
+        var CATID = $(".breadcrumb li:last").attr("class");
+        CATID = CATID.replace("category", "");
+        if ("product" != CATID) {
+            _4TellBoost.setPageType('Category');
+            _4TellBoost.setCatId(CATID);
+        }
+
+
+        if ($("#brands").length) {
+            _4TellBoost.setPageType('Home');
+        }
+    });
+
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
+//self-invoked namespace that protects $ and undefined internally
