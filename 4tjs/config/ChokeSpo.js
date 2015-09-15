f587c81ce76d7f175e33be360f229ec10789b53f
dougadams4 <dougadams4@gmail.com>
2015-09-15 01:46:50 -0700

    snapshot4tjsChokeSpo.js - initial pull

--- 4tjs/config/ChokeSpo.js
+++ 4tjs/config/ChokeSpo.js
@@ -16,14 +16,18 @@
         },
         PageSettings: {
             Home: [{
-                enable: true,
+                enable: false,
                 resultType: 1,
-                numItems: 9,
+                numItems: 4,
                 caption: "Recommended for You...",
                 productStyle: "product4T product4THome",
                 divSelect: "#HomeNewProducts",
                 divPosition: "replace",
-                numRows: 3,
+                //carousel: {
+                //    numVis: 4,
+                //    circular: true
+                //},
+                numRows: 2,
                 showRatings: false,
                 maxImageHeight: 150,
                 wrapper: "<div class='HOME4T'></div>",
@@ -35,14 +39,13 @@
                 numItems: 4,
                 caption: "customers also bought...",
                 productStyle: "product4T product4TPD2",
-                divSelect: "#SimilarProductsByCustomerViews",
+                divSelect: "#ProductTabs",
                 divPosition: "above",
                 //carousel: {
                 //    numVis: 4,
                 //    circular: true
                 //},
                 showRatings: false,
-                showBuyButton: false,
                 maxImageHeight: 150,
                 wrapper: "<div class='PD14T'></div>",
                 rawJS: {},
@@ -51,20 +54,17 @@
                 {
                     enable: true,
                     resultType: 3,
-                    numItems: 16,
-                    caption: "CUSTOMERS ALSO VIEWED",
+                    numItems: 4,
+                    caption: "CUSTOMERS WHO VIEWED THIS PRODUCT ALSO VIEWED",
                     productStyle: "product4T product4TPD2",
-                    divSelect: "#mytabs",
-                    divPosition: "below",
-                    //divSelect: "#SimilarProductsByCustomerViews",
-                    //divPosition: "replace",
+                    divSelect: "#SimilarProductsByCustomerViews",
+                    divPosition: "replace",
                     //carousel: {
                     //    numVis: 4,
                     //    circular: true
                     //},
                     maxImageHeight: 120,
                     showRatings: false,
-                    showBuyButton: false,
                     wrapper: "<div class='PD14T'></div>",
                     rawJS: {
                         preDisplay: function () {
@@ -72,31 +72,33 @@
                         }
                     },
                     inCart: false
-                }],
-            AddToCart: [{
-                enable: true,
-                resultType: 0,
-                numItems: 4,
-                caption: "You may also like...",
-                productStyle: "product4T product4TVC",
-                divSelect: ".CartCode > *:last",
-                divPosition: "below",
-                //carousel: {
-                //    numVis: 4,
-                //    circular: true
-                //},
-                maxImageHeight: 100,
-                showRatings: false,
-                showBuyButton: false,
-                wrapper: "<div class='VC4T'></div>",
-                rawJS: {
-                    preDisplay: function () {
-                        $(".ProceedToCheckout").insertAfter($(".CouponCode"));
-                        $(".KeepShopping").insertAfter($(".GiftCertificate "));
-                    }
-                },
-                inCart: true
-            }]
+                }
+            ],
+            AddToCart: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 4,
+                    caption: "You may also like...",
+                    productStyle: "product4T product4TVC",
+                    divSelect: ".CartCode > *:last",
+                    divPosition: "below",
+                    //carousel: {
+                    //    numVis: 4,
+                    //    circular: true
+                    //},
+                    maxImageHeight: 100,
+                    showRatings: false,
+                    wrapper: "<div class='VC4T'></div>",
+                    rawJS: {
+                        preDisplay: function () {
+                            $(".ProceedToCheckout").insertAfter($(".CouponCode"));
+                            $(".KeepShopping").insertAfter($(".GiftCertificate "));
+                        }
+                    },
+                    inCart: true
+                }
+            }
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
