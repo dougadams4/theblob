cc25c94300e6de750045a2ce4c9afd8420e0758b
dougadams4 <dougadams4@gmail.com>
2015-09-15 09:26:08 -0700

    snapshot4tjs3streetmo.js - initial pull

--- 4tjs3/config/streetmo.js
+++ 4tjs3/config/streetmo.js
@@ -10,14 +10,14 @@
             pricePrefix: "",
             salePricePrefix: "",
             includeBase: false,
-            siteEnable: false
+            siteEnable: true
         },
         PageSettings: {
             Home: [{
                 enable: true,
                 resultType: 1,
                 numItems: 12,
-                caption: "You May Like",
+                caption: "Recommended for You",
                 productStyle: "product4T product4THome",
                 divSelect: "#homePagePromo",
                 divPosition: "below",
@@ -28,7 +28,7 @@
                     navigationText: false,
                     pagination: false
                 },
-                maxImageHeight: 100,
+                maxImageHeight: 150,
                 showRatings: false,
                 showPrice: true,
                 wrapper: "<div class='HOME4T'></div>",
@@ -38,20 +38,21 @@
             ProductDetail: [{
                 enable: true,
                 resultType: 0,
-                numItems: 12,
-                caption: "Customers also bought",
+                numItems: 6,
+                caption: "Other Items That May Interest You",
                 productStyle: "product4T product4TPD1",
-                divSelect: ".desc",
+                divSelect: "ul.tabs",
                 divPosition: "above",
                 carousel: {
                     items: 2,
+                    itemsCustom: [[0, 2]],
                     scrollPerPage: true,
                     navigation: true,
                     navigationText: false,
                     pagination: false
                 },
                 showPrice: true,
-                showRatings: true,
+                showRatings: false,
                 maxImageHeight: 150,
                 wrapper: "<div class='PD14T'></div>",
                 rawJS: {
@@ -61,7 +62,30 @@
                         });
                     }
                 },
+                responsive: {
+                    1: {
+                        divSelect: "ul.tabs",
+                        divPosition: "above"
+                    },
+                    2: {
+                        divSelect: "ul.tabs",
+                        divPosition: "above"
+                    },
+                    3: {
+                        divSelect: ".eight.columns.alpha:has(div.desc)",
+                        divPosition: "above"
+                    },
+                    4: {
+                        divSelect: ".eight.columns.alpha:has(div.desc)",
+                        divPosition: "above"
+                    }
+                },
                 inCart: false
+            }, {
+                appendResults: true,
+                enable: true,
+                resultType: 3,
+                numItems: 6,
             }],
             Search: [{
                 enable: false
@@ -73,18 +97,19 @@
                 enable: true,
                 resultType: 0,
                 numItems: 12,
-                caption: "You may also like",
+                caption: "You May Also Like",
                 productStyle: "product4T product4TVC",
                 divSelect: "#cross-sell",
                 divPosition: "replace",
                 carousel: {
                     items: 3,
+                    itemsCustom: [[0, 3]],
                     scrollPerPage: true,
                     navigation: true,
                     navigationText: false,
                     pagination: false
                 },
-                maxImageHeight: 100,
+                maxImageHeight: 150,
                 showPrice: true,
                 showRatings: true,
                 wrapper: "<div class='VC4T'></div>",
@@ -94,7 +119,29 @@
                     }
                 },
                 inCart: true
+            }],
+            E404: [{
+                enable: true,
+                resultType: 1,
+                numItems: 12,
+                caption: "More Ideas",
+                productStyle: "product4T product4THome",
+                divSelect: "#ModaLisa",
+                divPosition: "below",
+                carousel: {
+                    items: 5,
+                    scrollPerPage: true,
+                    navigation: true,
+                    navigationText: false,
+                    pagination: false
+                },
+                maxImageHeight: 150,
+                showRatings: false,
+                showPrice: true,
+                wrapper: "<div class='EP4T'></div>",
+                rawJS: {},
+                inCart: false
             }]
-        }
+       }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
