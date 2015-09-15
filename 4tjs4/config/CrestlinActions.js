3bdd080e14891eb38cd5eac37e5df5eb9f218042
dougadams4 <dougadams4@gmail.com>
2015-09-15 01:59:16 -0700

    live4tjs4CrestlinActions.js - initial pull

---
+++ 4tjs4/config/CrestlinActions.js
@@ -0,0 +1,5 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+    _4TellBoost.Service.address = "stage.4-Tell.net/Boost2.0/";
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
+
