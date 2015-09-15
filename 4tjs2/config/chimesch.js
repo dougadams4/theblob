56b74d8c6f5799507c43ee4c1fcc778061541262
dougadams4 <dougadams4@gmail.com>
2015-09-15 01:38:10 -0700

    live4tjs2chimesch.js - initial pull

---
+++ 4tjs2/config/chimesch.js
@@ -0,0 +1,84 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.CONFIG = {
+        SiteInfo: {
+            baseURL: "www.CLIENTURL.com",
+            alias: "chimesch",
+            GA_UA: "UA-2382981-1",
+            platform: "4TellADN.js",
+            addCartBtnAtts: "<a class='ajax-add-product'>Add to Cart</a>",
+            addCartImage: "",
+            spacerImage: "/web/assets/images/spacer.gif",
+            emptyImage: "thumbnail.asp?file=/assets/images/default.jpg",
+            pricePrefix: "Our Price: ",
+            salePricePrefix: "On Sale: ",
+            includeBase: false,
+            siteEnable: true
+        },
+        PageSettings: {
+            Home: [{
+                enable: false
+            }],
+            ProductDetail: [{
+                enable: true,
+                resultType: 0,
+                numItems: 12,
+                caption: "Our customers also bought...",
+                productStyle: "product4T product4TPD1",
+                divSelect: "#tout1_pdp_4Tell",
+                divPosition: "replace",
+                carousel: {
+                    numVis: 3,
+                    circular: true
+                },
+                showRatings: true,
+                maxImageHeight: 150,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {
+                    preInit: function (tout) {
+                        _4TellBoost.addProductID($(tout.divSelect).data().products.toString())
+                    }
+                },
+                inCart: false
+            }, {
+                enable: false
+            }],
+            Search: [{
+                enable: false
+            }],
+            Category: [{
+                enable: true,
+                resultType: 5,
+                numItems: 4,
+                caption: "More ideas...",
+                productStyle: "product4T product4TCat",
+                divSelect: "#dickbutts",
+                divPosition: "append",
+                setHeight: false,
+                setWidth: false,
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
+                inCart: false
+            }],
+            AddToCart: [{
+                enable: true,
+                resultType: 0,
+                numItems: 4,
+                caption: "You may also like...",
+                productStyle: "product4T product4TVC",
+                divSelect: "#second4TellContainer",
+                divPosition: "append",
+                setHeight: true,
+                setWidth: false,
+                imageSize: "&maxx=100&maxy=100",
+                maxImageHeight: 100,
+                showRatings: true,
+                wrapper: "<div class='PD14T'></div>",
+                rawJS: {},
+                inCart: true
+            }]
+        }
+    }
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
