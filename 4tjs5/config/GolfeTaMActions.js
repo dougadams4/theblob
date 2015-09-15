99b237d323ed0aed0eeac05d4a86d5f3ea651dff
dougadams4 <dougadams4@gmail.com>
2015-09-15 03:27:00 -0700

    snapshot4tjs5GolfeTaMActions.js - initial pull

--- 4tjs5/config/GolfeTaMActions.js
+++ 4tjs5/config/GolfeTaMActions.js
@@ -2,7 +2,7 @@
     _4TellBoost.Service.customLoaded = true;
     $(document).ready(function () {
         var CATID = $(".breadcrumb li:last").attr("class");
-        CATID = CATID && CATID.replace("category", "");
+        CATID = CATID.replace("category", "");
         if ("product" != CATID) {
             _4TellBoost.setPageType('Category');
             _4TellBoost.setCatId(CATID);
@@ -13,20 +13,6 @@
             _4TellBoost.setPageType('Home');
         }
     });
-
-    _4TellBoost.getBuyBtn = function (tout, itemdata) {
-        var buyWrapper = $("<div class='productBuy' />");
-        var buyBtn = $(_4TellBoost.SiteInfo.addCartBtnAtts);
-        if (buyBtn)
-            buyBtn.appendTo(buyWrapper);
-        var newAddress = itemdata.pageLink;
-
-        //can't wrap with a-href in IE so add new address to the onclick handler
-        buyBtn.click(function () {
-            _4TellBoost.TrackClick(tout.toutType, itemdata.productID, newAddress);
-        });
-        return buyWrapper;
-    };
 
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
 //self-invoked namespace that protects $ and undefined internally
