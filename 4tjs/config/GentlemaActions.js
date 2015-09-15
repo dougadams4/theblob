3022c616a6e6955f495b46e1c42c4206c4f09c3c
dougadams4 <dougadams4@gmail.com>
2015-09-15 03:16:15 -0700

    snapshot4tjsGentlemaActions.js - initial pull

--- 4tjs/config/GentlemaActions.js
+++ 4tjs/config/GentlemaActions.js
@@ -1,6 +1,7 @@
 (function (_4TellBoost, $, undefined) {
     _4TellBoost.Service.customLoaded = true;
-       _4TellBoost.getBuyBtn = function (tout, itemdata) {
+    //_4TellBoost.Service.address = "stage.4-Tell.net/Boost2.0/";
+    _4TellBoost.getBuyBtn = function (tout, itemdata) {
         var buyWrapper = $("<div class='productBuy' />");
         var buyBtn = $(_4TellBoost.SiteInfo.addCartBtnAtts);
         if (_4TellBoost.SiteInfo.addCartImage) {
