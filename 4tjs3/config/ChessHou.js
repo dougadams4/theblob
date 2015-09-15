f2bcd2a8689e50b6c4d61e764b7653f64c95bc89
dougadams4 <dougadams4@gmail.com>
2015-09-15 01:35:08 -0700

    snapshot4tjs3ChessHou.js - initial pull

--- 4tjs3/config/ChessHou.js
+++ 4tjs3/config/ChessHou.js
@@ -15,32 +15,31 @@
             siteEnable: true
         },
         PageSettings: {
-            ProductDetail: [{
-                enable: true,
-                resultType: 0,
-                numItems: 6,
-                caption: "Customers also bought...",
-                productStyle: "product4T product4TPD1",
-                divSelect: "table.colors_pricebox, table.colors_backgroundneutral",
-                divPosition: "below",
-                carousel: {
-                    items: 3,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
+            ProductDetail: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 6,
+                    caption: "Customers also bought...",
+                    productStyle: "product4T product4TPD1",
+                    divSelect: "table.colors_pricebox, table.colors_backgroundneutral",
+                    divPosition: "below",
+                    carousel: {
+                        circular: true,
+                        numVis: 3
+                    },
+                    showRatings: true,
+                    maxImageHeight: 80,
+                    wrapper: "<table width='490' class='toutWrapper4T' style='margin-top: 10px;'><tr><td></td></tr></table>",
+                    rawJS: {
+                        preInit: function (tout) {
+                            if ($(".colors_pricebox").length > 1)
+                                tout.divSelect = "#ProductDetail_ProductDetails_div2";
+                        }
+                    },
+                    inCart: false
                 },
-                showRatings: true,
-                maxImageHeight: 80,
-                wrapper: "<div class='PD14T'></div>",
-                rawJS: {
-                    preDisplay: function (tout) {
-                        jQuery(tout.divSelect).parents("table").css("table-layout", "fixed");
-                    }
-                },
-                inCart: false
-            },
-                {
+                tout2: {
                     enable: true,
                     resultType: 3,
                     numItems: 12,
@@ -49,58 +48,60 @@
                     divSelect: "div#content_area table:has(table.colors_descriptionbox, a[href*=Reviews]):last + br",
                     divPosition: "below",
                     carousel: {
-                        items: 4,
-                        scrollPerPage: true,
-                        navigation: true,
-                        navigationText: false,
-                        pagination: false
+                        circular: true,
+                        numVis: 4
                     },
                     maxImageHeight: 80,
                     showRatings: true,
-                    wrapper: "<div class='PD14T'></div>",
+                    wrapper: "<table width='780' class='toutWrapper4T' style='margin-bottom:10px' align='center'>",
                     rawJS: {
                         preInit: function () {
                             $('table.colors_lines_light').hide();
                         }
                     },
                     inCart: false
-                }],
-            AddToCart: [{
-                enable: false,
-                resultType: 0,
-                numItems: 6,
-                caption: "You may also like...",
-                productStyle: "product4T product4TVC",
-                divSelect: "td#v65-cart-shipping-details-text > *",
-                divPosition: "below",
-                carousel: {
-                    items: 2,
-                    scrollPerPage: true,
-                    navigation: true,
-                    navigationText: false,
-                    pagination: false
-                },
-                maxImageHeight: 100,
-                showRatings: true,
-                wrapper: "<div class='PD14T'></div>",
-                rawJS: {
-                    preDisplay: function (tout) {
-                        jQuery(tout.divSelect).parents("table").css("table-layout", "fixed");
-                        $('td.v65-cart-details-blank[colspan=3]:last').hide();
-                        $(tout.divSelect).parent().attr('colspan', '4');
-                    }
-                },
-                inCart: true
-            }],
-            Home: [{
-                enable: false
-            }],
-            Category: [{
-                enable: false
-            }],
-            Search: [{
-                enable: false
-            }]
+                }
+            },
+            AddToCart: {
+                tout1: {
+                    enable: true,
+                    resultType: 0,
+                    numItems: 6,
+                    caption: "You may also like...",
+                    productStyle: "product4T product4TVC",
+                    divSelect: "td#v65-cart-shipping-details-text > *",
+                    divPosition: "below",
+                    carousel: {
+                        numVis: 2,
+                        circular: true
+                    },
+                    maxImageHeight: 100,
+                    showRatings: true,
+                    wrapper: "<div width='277px'></div>",
+                    rawJS: {
+                        preDisplay: function (tout) {
+                            $('td.v65-cart-details-blank[colspan=3]:last').hide();
+                            $(tout.divSelect).parent().attr('colspan', '4');
+                        }
+                    },
+                    inCart: true
+                }
+            },
+            Home: {
+                tout1: {
+                    enable: false
+                }
+            },
+            Category: {
+                tout1: {
+                    enable: false
+                }
+            },
+            Search: {
+                tout1: {
+                    enable: false
+                }
+            }
         }
     }
 }(window._4TellBoost = window._4TellBoost || {}, jQuery));
