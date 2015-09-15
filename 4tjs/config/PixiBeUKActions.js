e0ad559de0d6999bf09297e6a9d5aa763ab6e60d
dougadams4 <dougadams4@gmail.com>
2015-09-15 08:48:38 -0700

    live4tjsPixiBeUKActions.js - initial pull

---
+++ 4tjs/config/PixiBeUKActions.js
@@ -0,0 +1,30 @@
+(function (_4TellBoost, $, undefined) {
+    _4TellBoost.Service.customLoaded = true;
+
+    //Add to main 4TellMa
+    if (MagentoCartIDs) _4TellBoost.addCartItem(MagentoCartIDs.join());
+
+    _4TellBoost.getBuyBtn = function (tout, itemdata) {
+        var buyWrapper = $("<div class='productBuy' />");
+        var buyBtn = $(_4TellBoost.SiteInfo.addCartBtnAtts);
+        if (_4TellBoost.SiteInfo.addCartImage) {
+            var buyBtnImage = "";
+            if (_4TellBoost.SiteInfo.includeBase) {
+                buyBtnImage = "//" + _4TellBoost.SiteInfo.baseURL + "/"
+            }
+            buyBtnImage += _4TellBoost.SiteInfo.addCartImage;
+            buyBtn.attr("src", buyBtnImage)
+        }
+        if (buyBtn) {
+            buyBtn.appendTo(buyWrapper)
+        }
+        var newAddress = itemdata.pageLink;
+
+        buyBtn.click(function () {
+            _4TellBoost.addCartItem(itemdata.productID);
+            _4TellBoost.TrackClick(tout.toutType, itemdata.productID, newAddress);
+        });
+        return buyWrapper
+    };
+
+}(window._4TellBoost = window._4TellBoost || {}, jQuery));
